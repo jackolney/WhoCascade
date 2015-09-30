7aa85b9dccdd3a917bfc2e81f5f99a5b494679ee
Jack Olney <jack.olney11@imperial.ac.uk>
2015-09-30 21:41:49 +0100

    shiny setup

---
+++ ShinySetup.R
@@ -0,0 +1,13 @@
+# WhoCascade ShinySetup.R #
+# Jack Olney - 30/09/2015 #
+
+# Get devtools from Hadley
+# install.packages("devtools")
+require(devtools)
+
+# Get shinyapps from rstudio repo
+# install_github('rstudio/shinyapps')
+require(shinyapps)
+
+# Authorize account (not so fast!)
+# Details on shinyapps.io
