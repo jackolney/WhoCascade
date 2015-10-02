82ff194d9cea5541939336673af13e16a90f3ba6
Jack Olney <jack.olney11@imperial.ac.uk>
2015-10-02 09:08:35 +0100

    pdf preview

--- ShinyCascade/ui.R
+++ ShinyCascade/ui.R
@@ -64,7 +64,9 @@ shinyUI(
           )
         ),
     navbarMenu("More",
-    tabPanel("Sub-Component A"),
+    tabPanel("PDF",
+        HTML('<iframe src=\"https://drive.google.com/file/d/0B02uVauBTUwhd0U2cnY3dWJMcE0/preview\"style=\"border: 0; position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%\"></iframe>')
+        ),
     tabPanel("Raw Output",
         DT::dataTableOutput('outputTable')
         )
