6f52e48c9be6a5cc3c2cd3a0594219375217e25a
Jack Olney <jack.olney@me.com>
2015-10-06 15:58:18 +0100

    optimise on all three 90-90-90

--- optim/Complex-Model.R
+++ optim/Complex-Model.R
@@ -70,6 +70,7 @@ target <- 0.9
 # TEST #
 # ---- #
 
+# First the first 90.
 
 findFirst90 <- function(target, par) {
     
@@ -122,25 +123,30 @@ findFirst90 <- function(target, par) {
     results <- c(p_dx,p_tx,p_vs)
     definition <- c("% Diagnosed","% On Treatment","% Suppressed")
     the909090 <- data.frame(definition,results)
-    out <- (target - the909090$results[1])^2
+    out <- sum((target - the909090$results)^2)
+    # out <- (target - the909090$results[1])^2
     return(out)
 }
 
 theResult <- optim(par = 0, findFirst90, target = 0.9, lower = 0, upper = 10)
 theResult
 
-# Answer #
-theResult
-# ------ #
+# ----------------- #
+# FIND ALL 90-90-90 #
+# ----------------- #
+
+find909090 <- function(target, par) {
+    
+    print(paste("par = ",par))
 
     Parameters <- c(
         Nu_1 = 0.2139008,
         Nu_2 = 0.3379898,
         Nu_3 = 0.2744363,
-        Rho = 0.280838,
-        Gamma = 0.5,
-        Theta = 2,
-        Omega = 0.01,
+        Rho = par[1],
+        Gamma = par[2],
+        Theta = par[3],
+        Omega = par[4],
         Delta_1 = 1.1491019,
         Delta_2 = 2.5468165,
         Alpha_1 = 0.0043812,
@@ -152,8 +158,90 @@ theResult
         Tau_3 = 0.0631120,
         Tau_4 = 0.1225184,
         Mu = 0.0374,
-        Epsilon = 0.5
+        Epsilon = par[5]
     )
+
+    out <- ode(times=Time, y=Initial, func=ComplexCascade, parms=Parameters)
+    out <- tbl_df(data.frame(out))
+    out <- mutate(out,N = UnDx_500 + UnDx_350500 + UnDx_200350 + UnDx_200 + Dx_500 + Dx_350500 + Dx_200350 + Dx_200 + Care_500 + Care_350500 + Care_200350 + Care_200 + Tx_500 + Tx_350500 + Tx_200350 + Tx_200 + Vs_500 + Vs_350500 + Vs_200350 + Vs_200 + Ltfu_500 + Ltfu_350500 + Ltfu_200350 + Ltfu_200)
+    out <- mutate(out,ART = (Tx_500 + Tx_350500 + Tx_200350 + Tx_200 + Vs_500 + Vs_350500 + Vs_200350 + Vs_200) / N)
+    out <- mutate(out,UnDx = (UnDx_500 + UnDx_350500 + UnDx_200350 + UnDx_200) / N)
+    out <- mutate(out,Dx = (Dx_500 + Dx_350500 + Dx_200350 + Dx_200) / N)
+    out <- mutate(out,Care = (Care_500 + Care_350500 + Care_200350 + Care_200) / N)
+    out <- mutate(out,Tx = (Tx_500 + Tx_350500 + Tx_200350 + Tx_200) / N)
+    out <- mutate(out,Vs = (Vs_500 + Vs_350500 + Vs_200350 + Vs_200) / N)
+    out <- mutate(out,Ltfu = (Ltfu_500 + Ltfu_350500 + Ltfu_200350 + Ltfu_200) / N)
+    out <- mutate(out,NaturalMortalityProp = NaturalMortality / N)
+    out <- mutate(out,HivMortalityProp = HivMortality / N)
+    out <- mutate(out,NewInfProp = NewInf / N)
+
+
+    PLHIV = as.double(sum(filter(out,time == 5) %>% select(N)))
+    dx = as.double(sum(filter(out,time == 5) %>% select(c(Dx_500,Dx_350500,Dx_200350,Dx_200,Care_500,Care_350500,Care_200350,Care_200,Tx_500,Tx_350500,Tx_200350,Tx_200,Vs_500,Vs_350500,Vs_200350,Vs_200,Ltfu_500,Ltfu_350500,Ltfu_200350,Ltfu_200))))
+    tx = as.double(sum(filter(out,time == 5) %>% select(c(Tx_500,Tx_350500,Tx_200350,Tx_200,Vs_500,Vs_350500,Vs_200350,Vs_200))))
+    vs = as.double(sum(filter(out,time == 5) %>% select(c(Vs_500,Vs_350500,Vs_200350,Vs_200))))
+    p_dx <- dx / PLHIV
+    p_tx <- tx / dx
+    p_vs <- vs / tx
+    results <- c(p_dx,p_tx,p_vs)
+    definition <- c("% Diagnosed","% On Treatment","% Suppressed")
+    the909090 <- data.frame(definition,results)
+    out <- sum((target - the909090$results)^2)
+    # out <- (target - the909090$results[1])^2
+    return(out)
+}
+
+theResult <- optim(par = c(0,0,0,0,0), find909090, target = 0.9, lower = 0, upper = 10)
+theResult
+
+# Answer #
+theResult
+# ------ #
+
+    # Parameters <- c(
+    #     Nu_1 = 0.2139008,
+    #     Nu_2 = 0.3379898,
+    #     Nu_3 = 0.2744363,
+    #     Rho = 0.280838,
+    #     Gamma = 0.5,
+    #     Theta = 2,
+    #     Omega = 0.01,
+    #     Delta_1 = 1.1491019,
+    #     Delta_2 = 2.5468165,
+    #     Alpha_1 = 0.0043812,
+    #     Alpha_2 = 0.0179791,
+    #     Alpha_3 = 0.0664348,
+    #     Alpha_4 = 0.1289688,
+    #     Tau_1 = 0.0041621,
+    #     Tau_2 = 0.0170798,
+    #     Tau_3 = 0.0631120,
+    #     Tau_4 = 0.1225184,
+    #     Mu = 0.0374,
+    #     Epsilon = 0.5
+    # )
+
+    Parameters <- c(
+        Nu_1 = 0.2139008,
+        Nu_2 = 0.3379898,
+        Nu_3 = 0.2744363,
+        Rho = 0.2651495,
+        Gamma = 0.6180978,
+        Theta = 0.7914529,
+        Omega = 0,
+        Delta_1 = 1.1491019,
+        Delta_2 = 2.5468165,
+        Alpha_1 = 0.0043812,
+        Alpha_2 = 0.0179791,
+        Alpha_3 = 0.0664348,
+        Alpha_4 = 0.1289688,
+        Tau_1 = 0.0041621,
+        Tau_2 = 0.0170798,
+        Tau_3 = 0.0631120,
+        Tau_4 = 0.1225184,
+        Mu = 0.0374,
+        Epsilon = 1.4354894
+    )
+
 
     out <- ode(times=Time, y=Initial, func=ComplexCascade, parms=Parameters)
     out <- tbl_df(data.frame(out))
@@ -184,6 +272,8 @@ theResult
 levels(the909090$definition)
 the909090$definition <- factor(the909090$definition, levels=c("% Diagnosed","% On Treatment","% Suppressed"))
 the909090
+
+sum((target - the909090$results)^2)
 
 graphics.off()
 quartz.options(w=6,h=4)
