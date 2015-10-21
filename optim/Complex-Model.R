1cfa4b84a4f1b74ec445b8c0947e324a89983752
Jack Olney <jack.olney@me.com>
2015-10-21 13:57:47 +0100

    first iteration of solnp()

--- optim/Complex-Model.R
+++ optim/Complex-Model.R
@@ -52,7 +52,7 @@ Beta <- 0.0275837
 
 
 find909090 <- function(target, par) {
-    
+
     # print(paste("par =",par))
 
     Parameters <- c(
@@ -116,7 +116,7 @@ find909090 <- function(target, par) {
     # Returning outputs
     output <- 1/3 * sum((target - the909090$results)^2)
     # output <- sum((target - the909090$results)^2)
-    print(paste("error =",output,"cost =",dollar(cost)))    
+    print(paste("error =",output,"cost =",dollar(cost)))
     return(output)
 }
 
@@ -125,7 +125,7 @@ find909090 <- function(target, par) {
 
 # ModFit method
 require(FME)
-res <- modFit(find909090, 
+res <- modFit(find909090,
     p = c(
         Rho = 0.205,
         Gamma = 2.556,
@@ -135,8 +135,8 @@ res <- modFit(find909090,
         Sigma_a = 0.5,
         Phi = 3.628,
         Psi = 0.431,
-        Eta = 0.476), 
-    target = 0.9, 
+        Eta = 0.476),
+    target = 0.9,
     lower = c(
         Rho = 0.01,
         Gamma = 0.01,
@@ -146,7 +146,7 @@ res <- modFit(find909090,
         Sigma_a = 0.01,
         Phi = 0.01,
         Psi = 0.01,
-        Eta = 0.01), 
+        Eta = 0.01),
     upper = c(
         Rho = 20,
         Gamma = 20,
@@ -156,7 +156,7 @@ res <- modFit(find909090,
         Sigma_a = 20,
         Phi = 20,
         Psi = 20,
-        Eta = 20), 
+        Eta = 20),
     method = 'L-BFGS-B')
 res
 
@@ -174,7 +174,7 @@ parRange <- data.frame(
         Sigma_a = 0.01,
         Phi = 0.01,
         Psi = 0.01,
-        Eta = 0.01), 
+        Eta = 0.01),
     max = c(
         Rho = 20,
         Gamma = 20,
@@ -188,6 +188,21 @@ parRange <- data.frame(
 
 a <- Latinhyper(parRange,100)
 
+# For some interventions
+parRange <- data.frame(
+    off = c(
+        Testing = 0,
+        Linkage = 0
+        ),
+    on = c(
+        Testing = 1,
+        Linakge = 1
+        )
+    )
+
+# The number of combinations of 9 parameters that need testing?
+factorial(9)
+
 # Extract a value from LHC sample
 a[[1,"Rho"]]
 
@@ -196,6 +211,129 @@ a[[1,"Rho"]]
 # -> Help us minimise error towards 90-90-90 for the least money?
 # -> For a given amount of money, how can we maximise health benefit?
 # -> For a given health benefit, how cheap can it be?
+
+
+##############################
+install.packages("Rsolnp")
+require(Rsolnp)
+
+#specify your function
+# find909090() -> This is the function to optimise... i.e. aiming towards 90-90-90
+find909090 <- function(target, par) {
+
+    # print(paste("par =",par))
+
+    Parameters <- c(
+        Nu_1 = 0.193634,
+        Nu_2 = 0.321304,
+        Nu_3 = 0.163484,
+        Rho = par[["Rho"]],
+        Epsilon = par[["Epsilon"]],
+        Kappa = par[["Kappa"]],
+        Gamma = par[["Gamma"]],
+        Eta = par[["Eta"]],
+        Phi = par[["Phi"]],
+        Psi = par[["Psi"]],
+        Theta = 2.28,
+        Omega = par[["Omega"]],
+        p = 0.95,
+        s_1 = 0.25,
+        s_2 = 0.75,
+        s_3 = 1,
+        s_4 = 2,
+        Sigma_a = par[["Sigma_a"]],
+        Sigma_b = 0.5,
+        Delta_1 = 1.58084765,
+        Delta_2 = 3.50371789,
+        Alpha_1 = 0.00411,
+        Alpha_2 = 0.01167,
+        Alpha_3 = 0.01289,
+        Alpha_4 = 0.385832,
+        Tau_1 = 0.04013346,
+        Tau_2 = 0.05431511,
+        Tau_3 = 0.15692556,
+        Tau_4 = 0.23814569,
+        Mu = 0.0374,
+        Dx_unitCost = 10,
+        Care_unitCost = 12,
+        TxInit_unitCost = 28,
+        Retention_unitCost = 367,
+        AnnualTx_unitCost = 367
+    )
+
+    Time <- seq(0,5,0.02)
+    out <- ode(times=Time, y=Initial, func=ComplexCascade, parms=Parameters)
+    out <- tbl_df(data.frame(out))
+    out <- mutate(out,N = UnDx_500 + UnDx_350500 + UnDx_200350 + UnDx_200 + Dx_500 + Dx_350500 + Dx_200350 + Dx_200 + Care_500 + Care_350500 + Care_200350 + Care_200 + PreLtfu_500 + PreLtfu_350500 + PreLtfu_200350 + PreLtfu_200 + Tx_Na_500 + Tx_Na_350500 + Tx_Na_200350 + Tx_Na_200 + Tx_A_500 + Tx_A_350500 + Tx_A_200350 + Tx_A_200 + Vs_500 + Vs_350500 + Vs_200350 + Vs_200 + Ltfu_500 + Ltfu_350500 + Ltfu_200350 + Ltfu_200)
+    out <- mutate(out,TotalCost = Dx_Cost + Care_Cost + TxInit_Cost + Retention_Cost + AnnualTxCost)
+    PLHIV = as.double(sum(filter(out,time == 5) %>% select(N)))
+    dx = as.double(sum(filter(out,time == 5) %>% select(c(Dx_500,Dx_350500,Dx_200350,Dx_200,Care_500,Care_350500,Care_200350,Care_200,PreLtfu_500,PreLtfu_350500,PreLtfu_200350,PreLtfu_200,Tx_A_500,Tx_A_350500,Tx_A_200350,Tx_A_200,Tx_Na_500,Tx_Na_350500,Tx_Na_200350,Tx_Na_200,Vs_500,Vs_350500,Vs_200350,Vs_200,Ltfu_500,Ltfu_350500,Ltfu_200350,Ltfu_200))))
+    tx = as.double(sum(filter(out,time == 5) %>% select(c(Tx_A_500,Tx_A_350500,Tx_A_200350,Tx_A_200,Tx_Na_500,Tx_Na_350500,Tx_Na_200350,Tx_Na_200,Vs_500,Vs_350500,Vs_200350,Vs_200))))
+    vs = as.double(sum(filter(out,time == 5) %>% select(c(Vs_500,Vs_350500,Vs_200350,Vs_200))))
+    p_dx <- dx / PLHIV
+    p_tx <- tx / dx
+    p_vs <- vs / tx
+    results <- c(p_dx,p_tx,p_vs)
+    definition <- c("% Diagnosed","% On Treatment","% Suppressed")
+    the909090 <- data.frame(definition,results)
+
+    # Cost
+    cost <- as.double(sum(filter(out,time == 5) %>% select(TotalCost)))
+    print(paste("cost = ",cost))
+
+    # Returning outputs
+    output <- 1/3 * sum((target - the909090$results)^2)
+    # output <- sum((target - the909090$results)^2)
+    # print(paste("error =",output,"cost =",dollar(cost)))
+    return(output)
+}
+
+#specify the equality function. The number 15 (to which the function is equal)
+#is specified as an additional argument
+# This f(x) then should be the cost function. i.e. we set a cost?
+ForCost <- function(cost,target) {
+    print(cost)
+    cost[1]
+}
+
+#the optimiser - minimises by default (that is cool).
+solnp(pars = c(
+        Rho = 0.205,
+        Gamma = 2.556,
+        Epsilon = 16.949,
+        Omega = 0.033,
+        Kappa = 1.079,
+        Sigma_a = 0.5,
+        Phi = 3.628,
+        Psi = 0.431,
+        Eta = 0.476), # Starting values
+    find909090, # Function to optimise
+    eqfun=ForCost, # Equality function
+    eqB=100e+6,   # The equality constraint
+    LB = c(
+        Rho = 0.01,
+        Gamma = 0.01,
+        Epsilon = 0.01,
+        Omega = 0.01,
+        Kappa = 0.01,
+        Sigma_a = 0.01,
+        Phi = 0.01,
+        Psi = 0.01,
+        Eta = 0.01), # Lower bound for parameters i.e. greater than zero
+    UB = c(
+        Rho = 20,
+        Gamma = 20,
+        Epsilon = 20,
+        Omega = 20,
+        Kappa = 20,
+        Sigma_a = 20,
+        Phi = 20,
+        Psi = 20,
+        Eta = 20), # Upper bound for parameters (I just chose 100 randomly)
+        target = 0.9) # Additional arguments (should be given to both main and equality f(x) regardless of whether they are used by both or not).
+
+
+##############################
 
 # Perhaps an intervention just involves constraining the bounds of a parameter in a certain way? (but tightly-ish.)
 
@@ -216,7 +354,7 @@ params <- Latinhyper(par,100)
 for(i in 1:length(params)) {
 
     target <- 0.9
-    
+
     Parameters <- c(
         Nu_1 = 0.193634,
         Nu_2 = 0.321304,
@@ -253,7 +391,7 @@ for(i in 1:length(params)) {
         TxInit_unitCost = 28,
         Retention_unitCost = 367,
         AnnualTx_unitCost = 367
-    )    
+    )
 
     Time <- seq(0,5,0.02)
     out <- ode(times=Time, y=Initial, func=ComplexCascade, parms=Parameters)
@@ -278,7 +416,7 @@ for(i in 1:length(params)) {
     # Returning outputs
     output <- 1/3 * sum((target - the909090$results)^2)
     # output <- sum((target - the909090$results)^2)
-    print(paste("error =",output,"cost =",dollar(cost)))  
+    print(paste("error =",output,"cost =",dollar(cost)))
 
 }
 
@@ -286,26 +424,77 @@ for(i in 1:length(params)) {
 
 # Right now it is just reducing cost NOT focusing on getting to 90-90-90.
 
+res$par <- c(
+ Rho = 0.2050000,
+ Gamma = 2.6340549,
+ Epsilon = 16.9475865,
+ Omega = 0.0100000,
+ Kappa = 0.9487229,
+ Sigma_a = 0.7561403,
+ Phi = 3.6366206,
+ Psi = 0.5768007,
+ Eta = 0.8563607
+ )
+
+
+# Parameters <- c(
+#     Nu_1 = 0.193634,
+#     Nu_2 = 0.321304,
+#     Nu_3 = 0.163484,
+#     Rho = res$par[["Rho"]],
+#     Epsilon = res$par[["Epsilon"]],
+#     Kappa = res$par[["Kappa"]],
+#     Gamma = res$par[["Gamma"]],
+#     Eta = res$par[["Eta"]],
+#     Phi = res$par[["Phi"]],
+#     Psi = res$par[["Psi"]],
+#     Theta = 2.28,
+#     Omega = res$par[["Omega"]],
+#     p = 0.95,
+#     s_1 = 0.25,
+#     s_2 = 0.75,
+#     s_3 = 1,
+#     s_4 = 2,
+#     Sigma_a = res$par[["Sigma_a"]],
+#     Sigma_b = 0.5,
+#     Delta_1 = 1.58084765,
+#     Delta_2 = 3.50371789,
+#     Alpha_1 = 0.00411,
+#     Alpha_2 = 0.01167,
+#     Alpha_3 = 0.01289,
+#     Alpha_4 = 0.385832,
+#     Tau_1 = 0.04013346,
+#     Tau_2 = 0.05431511,
+#     Tau_3 = 0.15692556,
+#     Tau_4 = 0.23814569,
+#     Mu = 0.0374,
+#     Dx_unitCost = 10,
+#     Care_unitCost = 12,
+#     TxInit_unitCost = 28,
+#     Retention_unitCost = 367,
+#     AnnualTx_unitCost = 367
+# )
+
 
 Parameters <- c(
     Nu_1 = 0.193634,
     Nu_2 = 0.321304,
     Nu_3 = 0.163484,
-    Rho = res$par[["Rho"]],
-    Epsilon = res$par[["Epsilon"]],
-    Kappa = res$par[["Kappa"]],
-    Gamma = res$par[["Gamma"]],
-    Eta = res$par[["Eta"]],
-    Phi = res$par[["Phi"]],
-    Psi = res$par[["Psi"]],
+    Rho = 0.2050000,
+    Epsilon = 16.9475865,
+    Kappa = 0.9487229,
+    Gamma = 2.6340549,
+    Eta = 0.8563607,
+    Phi = 3.6366206,
+    Psi = 0.5768007,
     Theta = 2.28,
-    Omega = res$par[["Omega"]],
+    Omega = 0.0100000,
     p = 0.95,
     s_1 = 0.25,
     s_2 = 0.75,
     s_3 = 1,
     s_4 = 2,
-    Sigma_a = res$par[["Sigma_a"]],
+    Sigma_a = 0.7561403,
     Sigma_b = 0.5,
     Delta_1 = 1.58084765,
     Delta_2 = 3.50371789,
@@ -375,7 +564,7 @@ target <- 0.9
 # First the first 90.
 
 findFirst90 <- function(target, par) {
-    
+
     print(paste("par =",par))
 
     Parameters <- c(
@@ -438,7 +627,7 @@ theResult
 # ----------------- #
 
 find909090 <- function(target, par) {
-    
+
     print(paste("par =",par))
 
     Parameters <- c(
@@ -517,7 +706,7 @@ theResult
 require(FME)
 
 find909090_alt <- function(target, p) {
-    
+
     print(paste("p =",p))
 
     Parameters <- c(
