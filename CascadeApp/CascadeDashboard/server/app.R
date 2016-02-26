5b028580821de116d89daf0c3f44f4f43f72ace6
Jack Olney <jack.olney11@imperial.ac.uk>
2016-02-26 11:05:19 +0100

    simple alignment

--- CascadeApp/CascadeDashboard/server/app.R
+++ CascadeApp/CascadeDashboard/server/app.R
@@ -1,14 +1,14 @@
-source("server/alert.R", local = TRUE)
-source("server/data-table.R", local = TRUE)
-source("server/extract-data.R", local = TRUE)
-source("server/google-sheets.R", local = TRUE)
-source("server/observe-event.R", local = TRUE)
+source("server/alert.R",          local = TRUE)
+source("server/data-table.R",     local = TRUE)
+source("server/extract-data.R",   local = TRUE)
+source("server/google-sheets.R",  local = TRUE)
+source("server/observe-event.R",  local = TRUE)
 source("server/plot-functions.R", local = TRUE)
-source("server/plot.R", local = TRUE)
-source("server/reactive.R", local = TRUE)
-source("server/render.R", local = TRUE)
-source("server/table.R", local = TRUE)
-source("server/valuebox.R", local = TRUE)
+source("server/plot.R",           local = TRUE)
+source("server/reactive.R",       local = TRUE)
+source("server/render.R",         local = TRUE)
+source("server/table.R",          local = TRUE)
+source("server/valuebox.R",       local = TRUE)
 
 ## It appears as though no server code exists here.
 ## The reality is that it is all farmed out to other places.
@@ -22,18 +22,18 @@ source("server/valuebox.R", local = TRUE)
 # ----- #
 
 source("server/model/parameters.R", local = TRUE)
-source("server/model/initial.R", local = TRUE)
-source("server/model/beta.R", local = TRUE)
-source("server/model/model.R", local = TRUE)
+source("server/model/initial.R",    local = TRUE)
+source("server/model/beta.R",       local = TRUE)
+source("server/model/model.R",      local = TRUE)
 
 # ------------ #
 # OPTIMISATION #
 # ------------ #
 
-source("server/optimisation/functions.R", local = TRUE)
-source("server/optimisation/observe.R", local = TRUE)
-source("server/optimisation/optimise.R", local = TRUE)
-source("server/optimisation/parameters.R", local = TRUE)
+source("server/optimisation/functions.R",      local = TRUE)
+source("server/optimisation/observe.R",        local = TRUE)
+source("server/optimisation/optimise.R",       local = TRUE)
+source("server/optimisation/parameters.R",     local = TRUE)
 source("server/optimisation/plot-functions.R", local = TRUE)
-source("server/optimisation/plot.R", local = TRUE)
-source("server/optimisation/sim.R", local = TRUE)
+source("server/optimisation/plot.R",           local = TRUE)
+source("server/optimisation/sim.R",            local = TRUE)
