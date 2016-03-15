9e5b91cb94b7357a7c3c0e7b0af62ba88ab38b67
Jack Olney <jack.olney@me.com>
2016-03-15 15:31:02 +0000

    New CalibCallModel method

--- CascadeApp/CascadeDashboard/server/calibration/initial.R
+++ CascadeApp/CascadeDashboard/server/calibration/initial.R
@@ -1,69 +1,79 @@
-GetCalibInitial <- function(p, ...) {
+GetCalibInitial <- function(p, data) {
+    if(!is.list(data)) stop("Not a list.")
+
+    i2010            <- (dplyr::filter(data[["calib"]], year == 2010))
+    i2010_PLHIV      <- (dplyr::filter(i2010, indicator == "PLHIV")           %>% dplyr::select(value))[[1]]
+    i2010_PLHIV_Diag <- (dplyr::filter(i2010, indicator == "PLHIV Diagnosed") %>% dplyr::select(value))[[1]]
+    i2010_PLHIV_Care <- (dplyr::filter(i2010, indicator == "PLHIV in Care")   %>% dplyr::select(value))[[1]]
+    i2010_PLHIV_ART  <- (dplyr::filter(i2010, indicator == "PLHIV on ART")    %>% dplyr::select(value))[[1]]
+
+    iCD4_500    <- data[["cd4"]][1,"prop.Off.ART.500"][[1]]
+    iCD4_350500 <- data[["cd4"]][1,"prop.Off.ART.350500"][[1]]
+    iCD4_250350 <- data[["cd4"]][1,"prop.Off.ART.250350"][[1]]
+    iCD4_200250 <- data[["cd4"]][1,"prop.Off.ART.200250"][[1]]
+    iCD4_100200 <- data[["cd4"]][1,"prop.Off.ART.100200"][[1]]
+    iCD4_50100  <- data[["cd4"]][1,"prop.Off.ART.50100"][[1]]
+    iCD4_50     <- data[["cd4"]][1,"prop.Off.ART.50"][[1]]
+
     default <- initial(
         p,
-        UnDx_500 = 0,
-        UnDx_350500 = 0,
-        UnDx_250350 = 0,
-        UnDx_200250 = 0,
-        UnDx_100200 = 0,
+        UnDx_500 =       i2010_PLHIV * iCD4_500,
+        UnDx_350500 =    i2010_PLHIV * iCD4_350500,
+        UnDx_250350 =    i2010_PLHIV * iCD4_250350,
+        UnDx_200250 =    i2010_PLHIV * iCD4_200250,
+        UnDx_100200 =    i2010_PLHIV * iCD4_100200,
+        UnDx_50100 =     i2010_PLHIV * iCD4_50100,
+        UnDx_50 =        i2010_PLHIV * iCD4_50,
 
-        UnDx_50100 = 0,
-        UnDx_50 = 0,
-        Dx_500 = 0,
-        Dx_350500 = 0,
-        Dx_250350 = 0,
-        Dx_200250 = 0,
-        Dx_100200 = 0,
+        Dx_500 =         i2010_PLHIV_Diag * iCD4_500,
+        Dx_350500 =      i2010_PLHIV_Diag * iCD4_350500,
+        Dx_250350 =      i2010_PLHIV_Diag * iCD4_250350,
+        Dx_200250 =      i2010_PLHIV_Diag * iCD4_200250,
+        Dx_100200 =      i2010_PLHIV_Diag * iCD4_100200,
+        Dx_50100 =       i2010_PLHIV_Diag * iCD4_50100,
+        Dx_50 =          i2010_PLHIV_Diag * iCD4_50,
 
-        Dx_50100 = 0,
-        Dx_50 = 0,
-        Care_500 = 0,
-        Care_350500 = 0,
-        Care_250350 = 0,
-        Care_200250 = 0,
+        Care_500 =       i2010_PLHIV_Care * iCD4_500,
+        Care_350500 =    i2010_PLHIV_Care * iCD4_350500,
+        Care_250350 =    i2010_PLHIV_Care * iCD4_250350,
+        Care_200250 =    i2010_PLHIV_Care * iCD4_200250,
+        Care_100200 =    i2010_PLHIV_Care * iCD4_100200,
+        Care_50100 =     i2010_PLHIV_Care * iCD4_50100,
+        Care_50 =        i2010_PLHIV_Care * iCD4_50,
 
-        Care_100200 = 0,
-        Care_50100 = 0,
-        Care_50 = 0,
-        PreLtfu_500 = 0,
+        PreLtfu_500 =    0,
         PreLtfu_350500 = 0,
-
         PreLtfu_250350 = 0,
         PreLtfu_200250 = 0,
         PreLtfu_100200 = 0,
-        PreLtfu_50100 = 0,
-        PreLtfu_50 = 0,
+        PreLtfu_50100 =  0,
+        PreLtfu_50 =     0,
 
-        Tx_Na_500 = 0,
-        Tx_Na_350500 = 0,
-        Tx_Na_250350 = 0,
-        Tx_Na_200250 = 0,
-        Tx_Na_100200 = 0,
+        Tx_Na_500 =      i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_500,
+        Tx_Na_350500 =   i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_350500,
+        Tx_Na_250350 =   i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_250350,
+        Tx_Na_200250 =   i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_200250,
+        Tx_Na_100200 =   i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_100200,
+        Tx_Na_50100 =    i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_50100,
+        Tx_Na_50 =       i2010_PLHIV_ART * (1-p[["p"]]) * iCD4_50,
 
-        Tx_Na_50100 = 0,
-        Tx_Na_50 = 0,
-        Tx_A_500 = 0,
-        Tx_A_350500 = 0,
-        Tx_A_250350 = 0,
-        Tx_A_200250 = 0,
+        Tx_A_500 =       i2010_PLHIV_ART * p[["p"]] * iCD4_500,
+        Tx_A_350500 =    i2010_PLHIV_ART * p[["p"]] * iCD4_350500,
+        Tx_A_250350 =    i2010_PLHIV_ART * p[["p"]] * iCD4_250350,
+        Tx_A_200250 =    i2010_PLHIV_ART * p[["p"]] * iCD4_200250,
+        Tx_A_100200 =    i2010_PLHIV_ART * p[["p"]] * iCD4_100200,
+        Tx_A_50100 =     i2010_PLHIV_ART * p[["p"]] * iCD4_50100,
+        Tx_A_50 =        i2010_PLHIV_ART * p[["p"]] * iCD4_50,
 
-        Tx_A_100200 = 0,
-        Tx_A_50100 = 0,
-        Tx_A_50 = 0,
-        Ltfu_500 = 0,
-        Ltfu_350500 = 0,
-        Ltfu_250350 = 0,
-
-        Ltfu_200250 = 0,
-        Ltfu_100200 = 0,
-        Ltfu_50100 = 0,
-        Ltfu_50 = 0
+        Ltfu_500 =       0,
+        Ltfu_350500 =    0,
+        Ltfu_250350 =    0,
+        Ltfu_200250 =    0,
+        Ltfu_100200 =    0,
+        Ltfu_50100 =     0,
+        Ltfu_50 =        0
     )
-    replace <- c(...)
-    if(length(replace) > 0L) {
-        stopifnot(is.numeric(replace))
-        stopifnot(all(names(replace) %in% names(default)))
-        default[names(replace)] <- replace
-    }
     default
 }
+
+GetCalibInitial(p, KenyaData)
