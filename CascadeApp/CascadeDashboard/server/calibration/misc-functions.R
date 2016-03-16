0e54dc04b275af11e2886619bed6c45f57ccefb2
Jack Olney <jack.olney@me.com>
2016-03-16 16:27:48 +0000

    progress toward a calibration

---
+++ CascadeApp/CascadeDashboard/server/calibration/misc-functions.R
@@ -0,0 +1,200 @@
+# Simple function calls for various permutations of the model
+
+RunBaselineModel <- function() {
+
+    KenyaData <- GetMasterDataSet("Kenya")
+
+    time <- seq(0, 5, 1)
+    p <- parameters(
+        prop_preART_500    = KenyaData[["cd4"]][1,"prop.Off.ART.500"][[1]],
+        prop_preART_350500 = KenyaData[["cd4"]][1,"prop.Off.ART.350500"][[1]],
+        prop_preART_250350 = KenyaData[["cd4"]][1,"prop.Off.ART.250350"][[1]],
+        prop_preART_200250 = KenyaData[["cd4"]][1,"prop.Off.ART.200250"][[1]],
+        prop_preART_100200 = KenyaData[["cd4"]][1,"prop.Off.ART.100200"][[1]],
+        prop_preART_50100  = KenyaData[["cd4"]][1,"prop.Off.ART.50100"][[1]],
+        prop_preART_50     = KenyaData[["cd4"]][1,"prop.Off.ART.50"][[1]],
+        t_1 = ConvertYear(KenyaData[["treatment_guidelines"]][["more500"]]),
+        t_2 = ConvertYear(KenyaData[["treatment_guidelines"]][["less500"]]),
+        t_3 = ConvertYear(KenyaData[["treatment_guidelines"]][["less350"]]),
+        t_4 = ConvertYear(KenyaData[["treatment_guidelines"]][["less250"]]),
+        t_5 = ConvertYear(KenyaData[["treatment_guidelines"]][["less200"]])
+        )
+    y <- GetCalibInitial(p, KenyaData)
+    i <- incidence(as.double(KenyaData[["incidence"]]))
+
+
+    # THE GUTS #
+    result <- CallCalibModel(time, y, p, i)
+    df <- AssembleComparisonDataFrame(country = "Kenya", model = result, data = KenyaData)
+    original <- SSE(df)
+    # THE END #
+
+    # p1 <- ggplot(dplyr::filter(error, indicator == "PLHIV"), aes(x = year, y = value, group = source)) +
+    #     geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    # p2 <- ggplot(dplyr::filter(error, indicator == "PLHIV Diagnosed"), aes(x = year, y = value, group = source)) +
+    #     geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    # p3 <- ggplot(dplyr::filter(error, indicator == "PLHIV in Care"), aes(x = year, y = value, group = source)) +
+    #     geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    # p4 <- ggplot(dplyr::filter(error, indicator == "PLHIV on ART"), aes(x = year, y = value, group = source)) +
+    #     geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    # graphics.off()
+    # quartz.options(w = 10, h = 5)
+    # gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
+
+
+    # Without Error in data.frame #
+    error2 <- dplyr::filter(original, source != "error")
+    p1 <- ggplot(dplyr::filter(error2, indicator == "PLHIV"), aes(x = year, y = value, group = source)) +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    p2 <- ggplot(dplyr::filter(error2, indicator == "PLHIV Diagnosed"), aes(x = year, y = value, group = source)) +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    p3 <- ggplot(dplyr::filter(error2, indicator == "PLHIV in Care"), aes(x = year, y = value, group = source)) +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    p4 <- ggplot(dplyr::filter(error2, indicator == "PLHIV on ART"), aes(x = year, y = value, group = source)) +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    # graphics.off()
+    # quartz.options(w = 10, h = 5)
+    gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
+}
+
+RunCalibration <- function(iterations) {
+
+    KenyaData <- GetMasterDataSet("Kenya")
+
+    time <- seq(0, 5, 1)
+    p <- parameters(
+        prop_preART_500    = KenyaData[["cd4"]][1,"prop.Off.ART.500"][[1]],
+        prop_preART_350500 = KenyaData[["cd4"]][1,"prop.Off.ART.350500"][[1]],
+        prop_preART_250350 = KenyaData[["cd4"]][1,"prop.Off.ART.250350"][[1]],
+        prop_preART_200250 = KenyaData[["cd4"]][1,"prop.Off.ART.200250"][[1]],
+        prop_preART_100200 = KenyaData[["cd4"]][1,"prop.Off.ART.100200"][[1]],
+        prop_preART_50100  = KenyaData[["cd4"]][1,"prop.Off.ART.50100"][[1]],
+        prop_preART_50     = KenyaData[["cd4"]][1,"prop.Off.ART.50"][[1]],
+        t_1 = ConvertYear(KenyaData[["treatment_guidelines"]][["more500"]]),
+        t_2 = ConvertYear(KenyaData[["treatment_guidelines"]][["less500"]]),
+        t_3 = ConvertYear(KenyaData[["treatment_guidelines"]][["less350"]]),
+        t_4 = ConvertYear(KenyaData[["treatment_guidelines"]][["less250"]]),
+        t_5 = ConvertYear(KenyaData[["treatment_guidelines"]][["less200"]])
+        )
+    y <- GetCalibInitial(p, KenyaData)
+    i <- incidence(as.double(KenyaData[["incidence"]]))
+
+    min_term = 5
+    max_term = 0.1
+
+    parRange <- data.frame(
+        min = c(
+            rho     = p[["Rho"]]     * min_term,
+            epsilon = p[["Epsilon"]] * min_term,
+            kappa   = p[["Kappa"]]   * min_term,
+            gamma   = p[["Gamma"]]   * min_term,
+            theta   = p[["Theta"]]   * min_term,
+            omega   = p[["Omega"]]   * min_term,
+            mu      = p[["Mu"]]      * 1,
+            q       = if(p[["q"]] * min_term > 1) {1} else {p[["q"]] * min_term}
+        ),
+        max = c(
+            rho     = p[["Rho"]]     * max_term,
+            epsilon = p[["Epsilon"]] * max_term,
+            kappa   = p[["Kappa"]]   * max_term,
+            gamma   = p[["Gamma"]]   * max_term,
+            theta   = p[["Theta"]]   * max_term,
+            omega   = p[["Omega"]]   * max_term,
+            mu      = p[["Mu"]]      * 0,
+            q       = if(p[["q"]] * max_term > 1) {1} else {p[["q"]] * max_term}
+        )
+    )
+    parRange
+
+    lhs <- FME::Latinhyper(parRange, num = iterations)
+
+    # For a given length of the lhs (sample):
+        # - Update the parameter() [dont need to update initials or incidence vectors]
+        # - pass parameters() to CallModel -> AssembleDF -> SSE.
+        # - TIME THAT SHIT!
+    # p[["Mu"]] <- 0
+    error <- c()
+    for(k in 1:dim(lhs)[1]) {
+        # print(k)
+        p[["Rho"]]     <- lhs[,"rho"][k]
+        p[["Epsilon"]] <- lhs[,"epsilon"][k]
+        p[["Kappa"]]   <- lhs[,"kappa"][k]
+        p[["Gamma"]]   <- lhs[,"gamma"][k]
+        p[["Theta"]]   <- lhs[,"theta"][k]
+        p[["Omega"]]   <- lhs[,"omega"][k]
+        p[["Mu"]]   <- lhs[,"mu"][k]
+        p[["q"]]       <- lhs[,"q"][k]
+
+        out <- SSE(AssembleComparisonDataFrame(country = "Kenya", model = CallCalibModel(time, y, p, i), data = KenyaData))
+        error[k] <- sum(out[out$source == "error","value"])
+    }
+    # plot(error)
+
+    a <- order(error)[1:(iterations * 0.1)]
+
+    lhs[,"rho"][a]
+
+    out <- c()
+    for(l in 1:(iterations * 0.1)) {
+
+        p[["Rho"]]     <- lhs[,"rho"][a[l]]
+        p[["Epsilon"]] <- lhs[,"epsilon"][a[l]]
+        p[["Kappa"]]   <- lhs[,"kappa"][a[l]]
+        p[["Gamma"]]   <- lhs[,"gamma"][a[l]]
+        p[["Theta"]]   <- lhs[,"theta"][a[l]]
+        p[["Omega"]]   <- lhs[,"omega"][a[l]]
+        p[["q"]]       <- lhs[,"q"][a[l]]
+
+        iOut <- SSE(AssembleComparisonDataFrame(country = "Kenya", model = CallCalibModel(time, y, p, i), data = KenyaData))
+        out <- rbind(out, iOut)
+    }
+
+    # Find Minimums & Maximums
+    # OF ONLY MODEL
+    # FOR EACH INDICATORS
+    AppendMinMax <- function(data) {
+        uniqueIndicators <- unique(data$indicator)
+        uniqueYear <- unique(data$year)
+
+        for(m in 1:length(uniqueIndicators)) {
+            for(l in 1:length(uniqueYear)) {
+                data[data$year == uniqueYear[l] & data$indicator == uniqueIndicators[m],"min"] <- min(data[data$year == uniqueYear[l] & data$indicator == uniqueIndicators[m],]$value)
+                data[data$year == uniqueYear[l] & data$indicator == uniqueIndicators[m],"max"] <- max(data[data$year == uniqueYear[l] & data$indicator == uniqueIndicators[m],]$value)
+                }
+        }
+        data
+    }
+
+    outdata <- dplyr::filter(out, source == "data")
+
+    out_min_max <- AppendMinMax(out[out$source == "model",])
+
+    p1 <- ggplot(data = outdata[outdata$indicator == "PLHIV",], aes(x = year, y = value, group = source)) +
+        geom_ribbon(data = out_min_max[out_min_max$indicator == "PLHIV",], aes(x = year, ymin = min, ymax = max, group = source), fill = "grey70") +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+    p2 <- ggplot(data = outdata[outdata$indicator == "PLHIV Diagnosed",], aes(x = year, y = value, group = source)) +
+        geom_ribbon(data = out_min_max[out_min_max$indicator == "PLHIV Diagnosed",], aes(x = year, ymin = min, ymax = max, group = source), fill = "grey70") +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+
+    p3 <- ggplot(data = outdata[outdata$indicator == "PLHIV in Care",], aes(x = year, y = value, group = source)) +
+        geom_ribbon(data = out_min_max[out_min_max$indicator == "PLHIV in Care",], aes(x = year, ymin = min, ymax = max, group = source), fill = "grey70") +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+
+    p4 <- ggplot(data = outdata[outdata$indicator == "PLHIV on ART",], aes(x = year, y = value, group = source)) +
+        geom_ribbon(data = out_min_max[out_min_max$indicator == "PLHIV on ART",], aes(x = year, ymin = min, ymax = max, group = source), fill = "grey70") +
+        geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
+
+
+    gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
+}
--- CascadeApp/CascadeDashboard/server/calibration/non-shiny-calibration-test.R
+++ CascadeApp/CascadeDashboard/server/calibration/non-shiny-calibration-test.R
@@ -3,7 +3,7 @@
 # This script will be a template for future shiny versions of the model.
 # Ideally, will just be a case of drag and drop of functions.
 # Create all functions external to this script.
-
+rm(list=ls())
 setwd("~/git/WhoCascade/CascadeApp/CascadeDashboard")
 dir()
 
@@ -16,149 +16,25 @@ dir()
 # --------- #
 
 # source all the relevant files
-source("server/calibration/master.R", local = FALSE)
+source("server/calibration/master.R",  local = FALSE)
+source("server/calibration/initial.R", local = FALSE)
+source("server/calibration/model.R",   local = FALSE)
+source("server/calibration/error.R",   local = FALSE)
 
-# Test functions are all running and load "Kenya" data
-KenyaData <- GetMasterDataSet("Kenya")
-KenyaData
+# This contains simple function calls for the models in various permutations
+source("server/calibration/misc-functions.R", local = FALSE)
 
 # load 'cascade' package and ensure it is the latest build.
 devtools::load_all(pkg = "~/git/WhoCascade/cascade")
-
-# Run 'test'
 devtools::test(pkg = "~/git/WhoCascade/cascade")
 
-# Pull out functions used by the model
-source("server/calibration/initial.R", local = FALSE)
-time <- seq(0, 5, 1)
-p <- parameters(
-    prop_preART_500    = KenyaData[["cd4"]][1,"prop.Off.ART.500"][[1]],
-    prop_preART_350500 = KenyaData[["cd4"]][1,"prop.Off.ART.350500"][[1]],
-    prop_preART_250350 = KenyaData[["cd4"]][1,"prop.Off.ART.250350"][[1]],
-    prop_preART_200250 = KenyaData[["cd4"]][1,"prop.Off.ART.200250"][[1]],
-    prop_preART_100200 = KenyaData[["cd4"]][1,"prop.Off.ART.100200"][[1]],
-    prop_preART_50100  = KenyaData[["cd4"]][1,"prop.Off.ART.50100"][[1]],
-    prop_preART_50     = KenyaData[["cd4"]][1,"prop.Off.ART.50"][[1]],
-    t_1 = ConvertYear(KenyaData[["treatment_guidelines"]][["more500"]]),
-    t_2 = ConvertYear(KenyaData[["treatment_guidelines"]][["less500"]]),
-    t_3 = ConvertYear(KenyaData[["treatment_guidelines"]][["less350"]]),
-    t_4 = ConvertYear(KenyaData[["treatment_guidelines"]][["less250"]]),
-    t_5 = ConvertYear(KenyaData[["treatment_guidelines"]][["less200"]])
-    )
-y <- GetCalibInitial(p, KenyaData)
-i <- incidence(as.double(KenyaData[["incidence"]])) # Can only replace this array if you enter a replacement of the same length (7). Also, if not as.double() ode() throws an error.
+# Run baseline model (nothing fancy)
+RunBaselineModel()
 
-# Parameter update
-# beta <- GetBeta(y, p)
-
-# Treatment Guidelines (t_1 to t_5)
-# Need some assumptions here around WHO Stage / CD4 levels
-
-# ConvertYear to alter treatment_guidelines into something that the model can understand
-
-
-# source calibration model function (with setup and data.frame return)
-source("server/calibration/model.R", local = FALSE)
-
-# Running the model
-result <- CallCalibModel(time, y, p, i)
-
-# Feed values for "kenya" into calibration version of the model
-# Compare 'KenyaData' to 'result'
-
-# Test with one variable at a time.
-# The way I see this, is that we have a quartz window with 4 segments.
-# Each a comparison between model and data (eventually will be merged onto one plot).
-# Functions to show the error between model and data.
-
-# Function to calculate error in a data.frame
-# Throw this into the error.R file.
-source("server/calibration/error.R", local = FALSE)
-
-# this function is held in error.R
-df <- AssembleComparisonDataFrame(country = "Kenya", model = result, data = KenyaData)
-
-## Pass to SSE() and return an updated data.frame
-# Genius
-error <- SSE(df)
-
-p1 <- ggplot(dplyr::filter(error, indicator == "PLHIV"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-p2 <- ggplot(dplyr::filter(error, indicator == "PLHIV Diagnosed"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-p3 <- ggplot(dplyr::filter(error, indicator == "PLHIV in Care"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-p4 <- ggplot(dplyr::filter(error, indicator == "PLHIV on ART"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-graphics.off()
-quartz.options(w = 10, h = 5)
-gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
-
-
-# Without Error in data.frame #
-error2 <- dplyr::filter(error, source != "error")
-p1 <- ggplot(dplyr::filter(error2, indicator == "PLHIV"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-p2 <- ggplot(dplyr::filter(error2, indicator == "PLHIV Diagnosed"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-p3 <- ggplot(dplyr::filter(error2, indicator == "PLHIV in Care"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-p4 <- ggplot(dplyr::filter(error2, indicator == "PLHIV on ART"), aes(x = year, y = value, group = source)) +
-    geom_line() + geom_point(aes(color = indicator, shape = source), size = 3)
-
-graphics.off()
-quartz.options(w = 10, h = 5)
-gridExtra::grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
-
-# Pull out error.
-a <- dplyr::filter(error, source == "error")
-
-ggplot(a, aes(x = year, y = value)) + geom_point(aes(color = indicator), size = 3)
-
-# TOTAL ERROR! (to minimise)
-sum(a$value)
 
 #####################
 # START OF FUNCTION #
 #####################
-
-# GetCountryMasterData
-KenyaData <- GetMasterDataSet("Kenya")
-
-# Set some crap.
-time <- seq(0, 5, 1)
-p <- parameters(
-    prop_preART_500    = data[["cd4"]][1,"prop.Off.ART.500"][[1]],
-    prop_preART_350500 = data[["cd4"]][1,"prop.Off.ART.350500"][[1]],
-    prop_preART_250350 = data[["cd4"]][1,"prop.Off.ART.250350"][[1]],
-    prop_preART_200250 = data[["cd4"]][1,"prop.Off.ART.200250"][[1]],
-    prop_preART_100200 = data[["cd4"]][1,"prop.Off.ART.100200"][[1]],
-    prop_preART_50100  = data[["cd4"]][1,"prop.Off.ART.50100"][[1]],
-    prop_preART_50     = data[["cd4"]][1,"prop.Off.ART.50"][[1]],
-    t_1 = ConvertYear(data[["treatment_guidelines"]][["more500"]]),
-    t_2 = ConvertYear(data[["treatment_guidelines"]][["less500"]]),
-    t_3 = ConvertYear(data[["treatment_guidelines"]][["less350"]]),
-    t_4 = ConvertYear(data[["treatment_guidelines"]][["less250"]]),
-    t_5 = ConvertYear(data[["treatment_guidelines"]][["less200"]])
-    )
-y <- GetCalibInitial(p, data)
-i <- incidence(as.double(data[["incidence"]]))
-
-# The following parameters can then be updated on the fly.
-p[["Rho"]]
-p[["Epsilon"]]
-p[["q"]]
-p[["Kappa"]]
-p[["Gamma"]]
-p[["Theta"]]
-p[["Omega"]]
 
 # Need a SINGLE FUNCTION, that:
     # - Assembles model
@@ -166,32 +42,38 @@ p[["Omega"]]
     # - Runs model
     # - Analyses data & calculates ERROR
 
-ERROR <- function(data, p) {
-
-# data = MasterDataSet for a particular country
-# Idea is that this function will be called multiple times and therefore needs to be relatively fast.
-
-    out <- SSE(AssembleComparisonDataFrame(country = "Kenya", model = CallCalibModel(time, y, p, i), data = data))
-
-}
-
 # Hadleys new visual profiler (awesome, cool).
-profvis::profvis({
-    SSE(AssembleComparisonDataFrame(country = "Kenya", model = CallCalibModel(time, y, p, i), data = KenyaData))
-})
+# profvis::profvis({
+#     SSE(AssembleComparisonDataFrame(country = "Kenya", model = CallCalibModel(time, y, p, i), data = KenyaData))
+# })
 
 #############
 # WEDNESDAY #
 #############
 # Create a parameter set
 # Use: FME::Latinhyper
-help(Latinhyper)
 
-# Example
-parRange <- data.frame(min = c(0, 1, 2, 3), max = c(10, 9, 8, 7))
-rownames(parRange) <- c("par1", "par2", "par3", "par4")
-parRange
-FME::Latinhyper(parRange, num = 10)
+# WHAT assumptions do we make about model parameters here?
+# These are ALL based on the previous Kenya model - perhaps we should start with a more blank slate here?
+# We want a simple calibration, that is also fast.
+
+# BASELINE SIMULATION.
+# BREAK IT DOWN. ONLY LOOK AT ONE PARAMETER AT A TIME.
+# Start with RHO
+
+# Run Calibration Model
+RunCalibration(100)
+
+# Varying RHO pushing it down super far so that it is rediculously quick.
+
+1 / lhs[,"rho"][k]
+1 / lhs[,"epsilon"][k]
+1 / lhs[,"kappa"][k]
+1 / lhs[,"gamma"][k]
+1 / lhs[,"theta"][k]
+1 / lhs[,"omega"][k]
+lhs[,"q"][k]
+
 
 # We can then apply this to a function, i.e. SSE(AssembleComparisonDataFrame(CallCalibModel()))
 
