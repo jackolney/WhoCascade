1e5a5a4a9b7e7c8807ea0f856a0716e7309d328f
Jack Olney <jack.olney@me.com>
2016-03-23 15:36:55 +0000

    detail on ghost values

--- CascadeApp/CascadeDashboard/ui/setup/calibration.R
+++ CascadeApp/CascadeDashboard/ui/setup/calibration.R
@@ -8,14 +8,16 @@ tabItem(tabName = "calibration",
             collapsed = FALSE,
             plotOutput('plotCalibration', height = 'auto', width = 'auto')
         ),
+        # Row ONE
         fluidRow(
             box(width = 6,
                 solidheader = TRUE,
                 status = "primary",
                 background = "light-blue",
-                title = "Diagnosis Rate (py^-1)",
+                title = "Diagnosis Rate",
                 collapsible = TRUE,
-                collapsed = FALSE,
+                collapsed = TRUE,
+                "Rate at which undiagnosed individuals become diagnosed (py^-1).",
                 numericInput("test_DiagRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
                 numericInput("test_DiagRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
             ),
@@ -23,9 +25,85 @@ tabItem(tabName = "calibration",
                 solidheader = TRUE,
                 status = "primary",
                 background = "light-blue",
-                title = "Linkage Rate (py^-1)",
+                title = "Proportion Linking to Care",
                 collapsible = TRUE,
-                collapsed = FALSE,
+                collapsed = TRUE,
+                "Proportion of diagnosed individuals that link to care immediately.",
+                numericInput("test_LinkRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
+                numericInput("test_LinkRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
+            )
+        ),
+        # Row TWO
+        fluidRow(
+            box(width = 6,
+                solidheader = TRUE,
+                status = "primary",
+                background = "light-blue",
+                title = "ART Initiation Rate (In Care)",
+                collapsible = TRUE,
+                collapsed = TRUE,
+                "ART initiation rate for diagnosed individuals engaged in pre-ART care (py^-1).",
+                numericInput("test_DiagRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
+                numericInput("test_DiagRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
+            ),
+            box(width = 6,
+                solidheader = TRUE,
+                status = "primary",
+                background = "light-blue",
+                title = "ART Initiation Rate (Not In Care)",
+                collapsible = TRUE,
+                collapsed = TRUE,
+                "ART initiation rate individuals not currently engaged in pre-ART care. This is also a function of current CD4 count (py^-1).",
+                numericInput("test_LinkRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
+                numericInput("test_LinkRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
+            )
+        ),
+        # Row THREE
+        fluidRow(
+            box(width = 6,
+                solidheader = TRUE,
+                status = "primary",
+                background = "light-blue",
+                title = "Pre-ART Dropout Rate",
+                collapsible = TRUE,
+                collapsed = TRUE,
+                "Rate at which diagnosed individuals disengage from pre-ART care (py^-1).",
+                numericInput("test_DiagRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
+                numericInput("test_DiagRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
+            ),
+            box(width = 6,
+                solidheader = TRUE,
+                status = "primary",
+                background = "light-blue",
+                title = "ART Disengagement Rate",
+                collapsible = TRUE,
+                collapsed = TRUE,
+                "Rate at which individuals on treatment disengage from ART care (py^-1).",
+                numericInput("test_LinkRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
+                numericInput("test_LinkRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
+            )
+        ),
+        # Row FOUR
+        fluidRow(
+            box(width = 6,
+                solidheader = TRUE,
+                status = "primary",
+                background = "light-blue",
+                title = "Natural Mortality Rate",
+                collapsible = TRUE,
+                collapsed = TRUE,
+                "Non-HIV mortality rate applied to all individuals (py^-1).",
+                numericInput("test_DiagRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
+                numericInput("test_DiagRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
+            ),
+            box(width = 6,
+                solidheader = TRUE,
+                status = "primary",
+                background = "light-blue",
+                title = "Proportion Adhering to Treatment",
+                collapsible = TRUE,
+                collapsed = TRUE,
+                "Proportion of individuals initiating ART that adhere to treatment and become virally suppressed.",
                 numericInput("test_LinkRate_U","Upper:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%'),
                 numericInput("test_LinkRate_L","Lower:", value = 0, min = 0, max = 100, step = 1e-6, width = '100%')
             )
@@ -48,8 +126,9 @@ tabItem(tabName = "calibration",
             solidHeader = TRUE,
             title = "Confirm Calibration",
             "Hit 'Accept' if you are content with the calibration figure and 'ghost-values' presented. If you are not happy with either, then please edit the ghost-values and hit 'Repeat'",
-            bsButton(inputId = "calib_accept", label = "Accept", style = "success", size = "large", block = TRUE, icon = icon("check", class = "fa-lg fa-fw", lib = "font-awesome")),
-            bsButton(inputId = "calib_repeat", label = "Repeat", style = "danger", size = "large", block = TRUE, icon = icon("repeat", class = "fa-lg fa-fw", lib = "font-awesome"))
+            p(""),
+            bsButton(inputId = "calib_accept", label = "Accept", style = "success", size = "large", block = TRUE, icon = icon("check",  class = "fa-lg fa-fw", lib = "font-awesome")),
+            bsButton(inputId = "calib_repeat", label = "Repeat", style = "danger",  size = "large", block = TRUE, icon = icon("repeat", class = "fa-lg fa-fw", lib = "font-awesome"))
         )
     )
 )
