b6d28ca8adc9677261e5d6cb100ded95e9226ce5
Jack Olney <jack.olney@me.com>
2016-04-08 16:42:02 +0100

    some alignment

--- CascadeApp/CascadeDashboard/ui/sidebar.R
+++ CascadeApp/CascadeDashboard/ui/sidebar.R
@@ -15,24 +15,24 @@ dashboardSidebar(
             menuSubItem("Parameters",             tabName = "parameters")
             ),
         menuItem("Results", icon = icon("line-chart", class = "fa-lg fa-fw", lib = "font-awesome"),
-            menuSubItem("Your Cascade",          tabName = "your_cascade"),
-            menuSubItem("The Care Cascade",      tabName = "care_cascade"),
-            menuSubItem("The Power's Cascade",   tabName = "powers_cascade"),
-            menuSubItem("90-90-90",              tabName = "_909090"),
-            menuSubItem("Incidence / Mortality", tabName = "incidence_mortality")
+            menuSubItem("Your Cascade",           tabName = "your_cascade"),
+            menuSubItem("The Care Cascade",       tabName = "care_cascade"),
+            menuSubItem("The Power's Cascade",    tabName = "powers_cascade"),
+            menuSubItem("90-90-90",               tabName = "_909090"),
+            menuSubItem("Incidence / Mortality",  tabName = "incidence_mortality")
             ),
         menuItem("Optimisation", icon = icon("pie-chart", class = "fa-lg fa-fw", lib = "font-awesome"),
-            menuSubItem("Introduction",        tabName = "opt-intro"),
-            menuSubItem("Wizard",              tabName = "opt-wizard"),
-            menuSubItem("Cost",                tabName = "opt-cost"),
-            menuSubItem("Parameter Selection", tabName = "opt-parameter"),
-            menuSubItem("Results",             tabName = "opt-results"),
-            menuSubItem("Budget",              tabName = "opt-budget")
+            menuSubItem("Introduction",           tabName = "opt-intro"),
+            menuSubItem("Wizard",                 tabName = "opt-wizard"),
+            menuSubItem("Cost",                   tabName = "opt-cost"),
+            menuSubItem("Parameter Selection",    tabName = "opt-parameter"),
+            menuSubItem("Results",                tabName = "opt-results"),
+            menuSubItem("Budget",                 tabName = "opt-budget")
             ),
         menuItem("More", icon = icon("question", class = "fa-lg fa-fw", lib = "font-awesome"),
-            menuSubItem("Model Document", tabName = "model_document"),
-            menuSubItem("Single Plot",    tabName = "single_plot"),
-            menuSubItem("All Plots",      tabName = "all_plots")
+            menuSubItem("Model Document",         tabName = "model_document"),
+            menuSubItem("Single Plot",            tabName = "single_plot"),
+            menuSubItem("All Plots",              tabName = "all_plots")
             )
     )
 )
