f22e4246bd827c800c84fcf7347adeac67fc0dd7
Jack Olney <jack.olney@me.com>
2016-04-11 10:32:51 +0100

    Control structure spacing change.
    Why is Rich always right?

--- CascadeApp/CascadeDashboard/server/calibration/error.R
+++ CascadeApp/CascadeDashboard/server/calibration/error.R
@@ -6,22 +6,22 @@ AssembleComparisonDataFrame <- function(country, model, data) {
 
     # Create Model data.frame
     modelOutput <- data.frame()
-    for(i in 1:5) {
+    for (i in 1:5) {
         year <- model$time + 2010
         source <- "model"
-        if(i == 1) {
+        if (i == 1) {
             value <- model$N
             indicator <- "PLHIV"
-        } else if(i == 2) {
+        } else if (i == 2) {
             value <- model$Dx + model$Care + model$PreLtfu + model$Tx + model$Ltfu
             indicator <- "PLHIV Diagnosed"
-        } else if(i == 3) {
+        } else if (i == 3) {
             value <- model$Care + model$Tx
             indicator <- "PLHIV in Care"
-        } else if(i == 4) {
+        } else if (i == 4) {
             value <- model$Tx
             indicator <- "PLHIV on ART"
-        } else if(i == 5) {
+        } else if (i == 5) {
             value <- model$Vs
             indicator <- "PLHIV Suppressed"
         }
@@ -34,16 +34,16 @@ AssembleComparisonDataFrame <- function(country, model, data) {
     # Create Data data.frame
     dataOutput <- data.frame()
 
-    for(i in 1:5) {
-        if(i == 1) {
+    for (i in 1:5) {
+        if (i == 1) {
             dOutput <- data[["calib"]][data[["calib"]]$indicator == "PLHIV",]
-        } else if(i == 2) {
+        } else if (i == 2) {
             dOutput <- data[["calib"]][data[["calib"]]$indicator == "PLHIV Diagnosed",]
-        } else if(i == 3) {
+        } else if (i == 3) {
             dOutput <- data[["calib"]][data[["calib"]]$indicator == "PLHIV in Care",]
-        } else if(i == 4) {
+        } else if (i == 4) {
             dOutput <- data[["calib"]][data[["calib"]]$indicator == "PLHIV on ART",]
-        } else if(i == 5) {
+        } else if (i == 5) {
             dOutput <- data[["calib"]][data[["calib"]]$indicator == "PLHIV Suppressed",]
         }
         dOutput$source <- "data"
@@ -57,42 +57,42 @@ AssembleComparisonDataFrame <- function(country, model, data) {
 
 # This will need to be able to handle ALL FOUR ERRORS and return a neat data.frame of errors in return.
 SSE <- function(df) {
-    # if(!is.data.frame(df)) stop("Not passing a data frame.")
+    # if (!is.data.frame(df)) stop("Not passing a data frame.")
     uniqueIndicators <- unique(df$indicator)
-    for(i in 1:length(uniqueIndicators)) {
+    for (i in 1:length(uniqueIndicators)) {
         data <- df[df$indicator == uniqueIndicators[i],]
 
         uniqueYears <- unique(data$year)
 
         # Calculate number of values in dataset for particular indicator (N)
         N <- 0
-        for(j in 1:length(uniqueYears)) {
+        for (j in 1:length(uniqueYears)) {
             iD <- data[data$year == uniqueYears[j] & data$source == "data","value"]
-            if(isEmpty(iD)) {
+            if (isEmpty(iD)) {
                 next
-            } else if(!is.na(iD)) {
+            } else if (!is.na(iD)) {
                 N <- N + 1
             }
         }
 
-        for(j in 1:length(uniqueYears)) {
+        for (j in 1:length(uniqueYears)) {
             iYr <- data[data$year == uniqueYears[j],]
 
             iData  <- iYr[iYr$source == "data","value"]
-            if(isEmpty(iData)) next
-            if(any(is.na(iData))) next
-            if(length(iData) > 1) {
+            if (isEmpty(iData)) next
+            if (any(is.na(iData))) next
+            if (length(iData) > 1) {
                 iData <- mean(iData)
                 warning("iData length > 1")
             }
 
             iWeight <- iYr[iYr$source == "data","weight"]
-            if(isEmpty(iWeight)) next
-            if(iWeight == "green") {
+            if (isEmpty(iWeight)) next
+            if (iWeight == "green") {
                 w <- 1
-            } else if(iWeight == "amber") {
+            } else if (iWeight == "amber") {
                 w <- 0.5
-            } else if(iWeight == "red") {
+            } else if (iWeight == "red") {
                 w <- 0.1
             }
 
