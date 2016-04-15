5cff743d6c754d40e627d4fde4842cb408485ecb
Jack Olney <jack.olney@me.com>
2016-04-15 15:40:22 +0100

    calibration error adjustment

--- CascadeApp/CascadeDashboard/server/calibration/plot.R
+++ CascadeApp/CascadeDashboard/server/calibration/plot.R
@@ -3,6 +3,6 @@ output$plotCalibration <- renderPlot({
     input$calib_repeat
 
     # RunCalibration(data = MasterData, maxIterations = 1e4, maxError = input$maxError, limit = input$minResults)
-    RunCalibration(data = MasterData, maxIterations = 1e4, maxError = input$maxError, limit = 10)
+    RunCalibration(data = MasterData, maxIterations = 1e4, maxError = input$maxError, limit = 100)
 
 }, height = 800, width = 'auto', bg = 'transparent')
