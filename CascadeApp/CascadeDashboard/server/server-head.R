6c209f32a8319a81d12dea31e24ab9f9a427acc7
Jack Olney <jack.olney@me.com>
2016-05-05 12:19:01 +0100

    package library location switch for OS

--- CascadeApp/CascadeDashboard/server/server-head.R
+++ CascadeApp/CascadeDashboard/server/server-head.R
@@ -1,18 +1,25 @@
 # devtools::install_github("jackolney/WhoCascade/cascade", ref = "master")
 # devtools::load_all(pkg = "~/git/WhoCascade/cascade")
-library(cascade)
-library(deSolve)
-library(dplyr)
-library(DT)
-library(ggplot2)
-library(googlesheets)
-library(grid)
-library(gridExtra)
-library(RColorBrewer)
-library(readr)
-library(rmarkdown)
-library(scales)
-library(shiny)
-library(shinyjs)
-library(testthat)
-library(V8)
+
+if (version$os == "darwin13.4.0") {
+    loc <- "/Library/Frameworks/R.framework/Versions/3.3/Resources/library"
+} else if (version$os == "linux-gnu") {
+    loc <- "/home/DIDE/jjo11/R/x86_64-pc-linux-gnu-library/3.2/"
+}
+
+library(cascade,      lib.loc = loc)
+library(deSolve,      lib.loc = loc)
+library(dplyr,        lib.loc = loc)
+library(DT,           lib.loc = loc)
+library(ggplot2,      lib.loc = loc)
+library(googlesheets, lib.loc = loc)
+library(grid,         lib.loc = loc)
+library(gridExtra,    lib.loc = loc)
+library(RColorBrewer, lib.loc = loc)
+library(readr,        lib.loc = loc)
+library(rmarkdown,    lib.loc = loc)
+library(scales,       lib.loc = loc)
+library(shiny,        lib.loc = loc)
+library(shinyjs,      lib.loc = loc)
+library(testthat,     lib.loc = loc)
+library(V8,           lib.loc = loc)
--- CascadeApp/CascadeDashboard/ui/uihead.R
+++ CascadeApp/CascadeDashboard/ui/uihead.R
@@ -1,16 +1,23 @@
 # devtools::install_github("jackolney/shinydashboard", ref = "master")
 # devtools::load_all(pkg = "~/git/packages/shinydashboard")
-library(DT)
-library(ggplot2)
-library(leaflet)
-library(rgdal)
-library(shiny)
-library(shinyBS)
-library(shinydashboard)
-library(shinyjs)
-library(shinyTable)
-library(shinythemes)
-library(V8)
+
+if (version$os == "darwin13.4.0") {
+    loc <- "/Library/Frameworks/R.framework/Versions/3.3/Resources/library"
+} else if (version$os == "linux-gnu") {
+    loc <- "/home/DIDE/jjo11/R/x86_64-pc-linux-gnu-library/3.2/"
+}
+
+library(DT,             lib.loc = loc)
+library(ggplot2,        lib.loc = loc)
+library(leaflet,        lib.loc = loc)
+library(rgdal,          lib.loc = loc)
+library(shiny,          lib.loc = loc)
+library(shinyBS,        lib.loc = loc)
+library(shinydashboard, lib.loc = loc)
+library(shinyjs,        lib.loc = loc)
+library(shinyTable,     lib.loc = loc)
+library(shinythemes,    lib.loc = loc)
+library(V8,             lib.loc = loc)
 
 # source global-lists
 source("ui/global-lists.R", local = TRUE)
