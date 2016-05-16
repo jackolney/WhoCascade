fb8ce3197ea8e9219af7ec3b1172c1e2cc8c79ae
Jack Olney <jack.olney@me.com>
2016-05-16 14:54:04 +0100

    alert events

--- CascadeApp/CascadeDashboard/server/alert.R
+++ CascadeApp/CascadeDashboard/server/alert.R
@@ -19,3 +19,70 @@ addPopover(session, id = "plotOpt909090",
     placement = "bottom",
     trigger = "hover",
     options = NULL)
+
+## Setup Alerts
+# Designed to catch users trying to enter non-sensical data
+
+observeEvent(input$uDIAG, {
+    if(input$uDIAG > input$uPLHIV) {
+        # shinyBS alert
+        createAlert(session,
+            anchorId = "uDIAG_ALERT",
+            alertId = NULL,
+            title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "WARNING"),
+            content = "Value entered for 'PLHIV Diagnosed' exceeds the value of 'PLHIV' in this year.",
+            style = "danger",
+            dismiss = TRUE,
+            append = TRUE)
+        # JS alert
+        shinyjs::info("Computer says no.")
+    } else return()
+})
+
+observeEvent(input$uCARE, {
+    if(input$uCARE > input$uDIAG) {
+        # shinyBS alert
+        createAlert(session,
+            anchorId = "uCARE_ALERT",
+            alertId = NULL,
+            title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "WARNING"),
+            content = "Value entered for 'PLHIV in Care' exceeds the value of 'PLHIV Diagnosed' in this year.",
+            style = "danger",
+            dismiss = TRUE,
+            append = TRUE)
+        # JS alert
+        shinyjs::info("Computer says no.")
+    } else return()
+})
+
+observeEvent(input$uART, {
+    if(input$uART > input$uCARE) {
+        # shinyBS alert
+        createAlert(session,
+            anchorId = "uART_ALERT",
+            alertId = NULL,
+            title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "WARNING"),
+            content = "Value entered for 'PLHIV on ART' exceeds the value of 'PLHIV in Care' in this year.",
+            style = "danger",
+            dismiss = TRUE,
+            append = TRUE)
+        # JS alert
+        shinyjs::info("Computer says no.")
+    } else return()
+})
+
+observeEvent(input$uVIRAL, {
+    if(input$uVIRAL > input$uART) {
+        # shinyBS alert
+        createAlert(session,
+            anchorId = "uVIRAL_ALERT",
+            alertId = NULL,
+            title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "WARNING"),
+            content = "Value entered for 'PLHIV Virally Suppressed' exceeds the value of 'PLHIV on ART' in this year.",
+            style = "danger",
+            dismiss = TRUE,
+            append = TRUE)
+        # JS alert
+        shinyjs::info("Computer says no.")
+    } else return()
+})
