88a171763bdd1797bf715e0d6b3adebecf784eed
Jack Olney <jack.olney@me.com>
2016-05-18 17:23:07 +0100

    allowing an update to input$minResults to re-run calibration

--- CascadeApp/CascadeDashboard/server/calibration/plot.R
+++ CascadeApp/CascadeDashboard/server/calibration/plot.R
@@ -2,8 +2,5 @@ output$plotCalibration <- renderPlot({
     # Dependency on 'REPEAT_calib' being hit
     # When 'repeat' button pressed, then we re-run calibration
     input$REPEAT_calib
-
-    # RunCalibration(data = MasterData, maxIterations = 1e4, maxError = input$maxError, limit = input$minResults)
-    RunCalibration(data = MasterData, maxIterations = 1e4, maxError = input$maxError, limit = 100)
-
+    RunCalibration(data = MasterData, maxIterations = 1e4, maxError = input$maxError, limit = input$minResults)
 }, height = 800, width = 'auto', bg = 'transparent')
