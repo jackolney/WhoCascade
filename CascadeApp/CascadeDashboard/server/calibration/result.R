59e83de56e1b0ec2a232b4a39efceb94fa7bc848
Jack Olney <jack.olney@me.com>
2016-05-19 14:37:06 +0100

    reactive mu removal

--- CascadeApp/CascadeDashboard/server/calibration/result.R
+++ CascadeApp/CascadeDashboard/server/calibration/result.R
@@ -8,7 +8,6 @@ CalibParamMaxMin$kappa_MAX   <- 0
 CalibParamMaxMin$gamma_MAX   <- 0
 CalibParamMaxMin$theta_MAX   <- 0
 CalibParamMaxMin$omega_MAX   <- 0
-CalibParamMaxMin$mu_MAX      <- 0
 CalibParamMaxMin$p_MAX       <- 0
 CalibParamMaxMin$q_MAX       <- 0
 
@@ -19,7 +18,6 @@ CalibParamMaxMin$kappa_MIN   <- 0
 CalibParamMaxMin$gamma_MIN   <- 0
 CalibParamMaxMin$theta_MIN   <- 0
 CalibParamMaxMin$omega_MIN   <- 0
-CalibParamMaxMin$mu_MIN      <- 0
 CalibParamMaxMin$p_MIN       <- 0
 CalibParamMaxMin$q_MIN       <- 0
 
@@ -31,6 +29,5 @@ userParRange$kappa   <- NA
 userParRange$gamma   <- NA
 userParRange$theta   <- NA
 userParRange$omega   <- NA
-userParRange$mu      <- NA
 userParRange$p       <- NA
 userParRange$q       <- NA
