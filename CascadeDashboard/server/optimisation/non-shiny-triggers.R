f46ff9c416db6c29c1f44a6ab656b85d87003cf6
Jack Olney <jack.olney@me.com>
2016-06-02 18:00:24 +0100

    optimisation plot test thoughts

--- CascadeDashboard/server/optimisation/non-shiny-triggers.R
+++ CascadeDashboard/server/optimisation/non-shiny-triggers.R
@@ -678,10 +678,16 @@ length(a)
 ### THIS IS NEW AND THIS IS COOL!!! ###
 #######################################
 
+test <- subset(theOut, theOut$VS > 0)
+dim(test)
 hi <- test
 
+bestPar <- GetBestPar(
+        masterCD4 = MasterCD4_2015,
+        data = MasterData,
+        calibParamOut = CalibParamOut,
+        minErrorRun = minErrorRun)
 
-subset(hi, hi$Rho / bestPar[["Rho"]] > 1)
 
 # Add columns to data.frame
 hi$testing <- hi$Rho / bestPar[["Rho"]]
@@ -690,14 +696,6 @@ hi$preRetention <- bestPar[["Kappa"]] / hi$Kappa
 hi$initiation <- hi$Gamma / bestPar[["Gamma"]]
 hi$adherence <- hi$Sigma
 hi$retention <- bestPar[["Omega"]] / hi$Omega
-
-
-mean(hi[hi$testing > 1, "testing"] * hi[hi$testing > 1, "VS"])
-mean(hi[hi$linkage > 1, "linkage"] * hi[hi$linkage > 1, "VS"])
-mean(hi[hi$preRetention > 1, "preRetention"] * hi[hi$preRetention > 1, "VS"])
-mean(hi[hi$initiation > 1, "initiation"] * hi[hi$initiation > 1, "VS"])
-mean(hi[hi$adherence > 1, "adherence"] * hi[hi$adherence > 1, "VS"])
-mean(hi[hi$retention > 1, "retention"] * hi[hi$retention > 1, "VS"])
 
 intervention <- c(
     "Testing",
@@ -716,8 +714,289 @@ strength <- c(
     mean(hi[hi$retention > 1, "retention"] * hi[hi$retention > 1, "VS"])
 )
 
+strength2 <- c(
+    mean(hi[, "testing"] * hi[, "VS"]),
+    mean(hi[, "linkage"] * hi[, "VS"]),
+    mean(hi[, "preRetention"] * hi[, "VS"]),
+    mean(hi[, "initiation"] * hi[, "VS"]),
+    mean(hi[, "adherence"] * hi[, "VS"]),
+    mean(hi[, "retention"] * hi[, "VS"])
+)
+
+strength3 <- c(
+    sum(hi[hi$testing > 1, "testing"] * hi[hi$testing > 1, "VS"]) / dim(hi)[1],
+    sum(hi[hi$linkage > 1, "linkage"] * hi[hi$linkage > 1, "VS"])  / dim(hi)[1],
+    sum(hi[hi$preRetention > 1, "preRetention"] * hi[hi$preRetention > 1, "VS"])  / dim(hi)[1],
+    sum(hi[hi$initiation > 1, "initiation"] * hi[hi$initiation > 1, "VS"])  / dim(hi)[1],
+    sum(hi[hi$adherence > 1, "adherence"] * hi[hi$adherence > 1, "VS"]) / dim(hi)[1],
+    sum(hi[hi$retention > 1, "retention"] * hi[hi$retention > 1, "VS"])  / dim(hi)[1]
+)
+
 # build data.frame
 resultOut <- data.frame(intervention, strength)
-ggplot(resultOut, aes(x = intervention, y = strength)) + geom_bar(stat = "identity")
+resultOut2 <- data.frame(intervention, strength2)
+resultOut3 <- data.frame(intervention, strength3)
 
+resultOut$intervention <- factor(resultOut$intervention, levels = intervention)
+resultOut2$intervention <- factor(resultOut2$intervention, levels = intervention)
+resultOut3$intervention <- factor(resultOut3$intervention, levels = intervention)
+
+
+a = ggplot(resultOut, aes(x = intervention, y = strength)) + geom_bar(stat = "identity")
+b = ggplot(resultOut2, aes(x = intervention, y = strength2)) + geom_bar(stat = "identity")
+c = ggplot(resultOut3, aes(x = intervention, y = strength3)) + geom_bar(stat = "identity")
+
+gridExtra::grid.arrange(a, b, c, nrow = 1, ncol = 3)
+
+
+
+# Thursday
+
+just take a subset
+
+section <- hi[1:10,]
+
+mean(section[section$testing > 1, "testing"] * section[section$testing > 1, "VS"])
+
+section[section$testing > 1, "testing"]
+section[section$testing > 1, "VS"]
+
+mean(hi[hi$adherence > 1, "adherence"] * hi[hi$adherence > 1, "VS"])
+
+
+Viral <- 0
+Viral <- Viral + 1
+# Subset data using opt_VS_cutoff
+selectedResults <- subset(theOut, theOut$VS >= (Viral / 10))
+dim(selectedResults)
+
+selectedResults$testing      <- selectedResults$Rho   / bestPar[["Rho"]]
+selectedResults$linkage      <- selectedResults$Q     / bestPar[["q"]]
+selectedResults$preRetention <- bestPar[["Kappa"]]    / selectedResults$Kappa
+selectedResults$initiation   <- selectedResults$Gamma / bestPar[["Gamma"]]
+selectedResults$adherence    <- selectedResults$Sigma
+selectedResults$retention    <- bestPar[["Omega"]]    / selectedResults$Omega
+
+intervention <- c(
+    "Testing",
+    "Linkage",
+    "Pre-ART Retention",
+    "Initiation",
+    "Adherence",
+    "ART Retention")
+
+strength <- c(
+    sum(selectedResults[selectedResults$testing      > 1, "testing"]      * selectedResults[selectedResults$testing      > 1, "VS"]) / dim(selectedResults)[1],
+    sum(selectedResults[selectedResults$linkage      > 1, "linkage"]      * selectedResults[selectedResults$linkage      > 1, "VS"]) / dim(selectedResults)[1],
+    sum(selectedResults[selectedResults$preRetention > 1, "preRetention"] * selectedResults[selectedResults$preRetention > 1, "VS"]) / dim(selectedResults)[1],
+    sum(selectedResults[selectedResults$initiation   > 1, "initiation"]   * selectedResults[selectedResults$initiation   > 1, "VS"]) / dim(selectedResults)[1],
+    sum(selectedResults[selectedResults$adherence    > 1, "adherence"]    * selectedResults[selectedResults$adherence    > 1, "VS"]) / dim(selectedResults)[1],
+    sum(selectedResults[selectedResults$retention    > 1, "retention"]    * selectedResults[selectedResults$retention    > 1, "VS"]) / dim(selectedResults)[1]
+)
+
+# build data.frame
+resultOut <- data.frame(intervention, strength)
+
+# Check level order
+resultOut$intervention <- factor(resultOut$intervention, levels = intervention)
+
+# This show the proportion of selected runs that use a particular intervention.
+ggOut <- ggplot(resultOut, aes(x = intervention, y = strength))
+ggOut <- ggOut + geom_bar(aes(fill = intervention), stat = "identity")
+ggOut <- ggOut + theme_classic()
+ggOut <- ggOut + ggtitle(
+    label = paste("VS =", Viral),
+    subtitle =
+        paste0("Percentage increase brought about by ",
+            scales::comma(dim(selectedResults)[1]),
+            " interventions, achieving ",
+            input$opt_VS_cutoff,
+            "% viral suppression"
+        )
+    )
+ggOut <- ggOut + theme(legend.position = "none")
+ggOut <- ggOut + theme(axis.text.x = element_text(size = 14))
+ggOut <- ggOut + theme(axis.text.y = element_text(size = 14))
+ggOut <- ggOut + theme(title =       element_text(size = 15))
+ggOut <- ggOut + theme(axis.title.y = element_blank())
+ggOut <- ggOut + theme(axis.title.x = element_blank())
+ggOut <- ggOut + theme(axis.line.y = element_line())
+ggOut <- ggOut + scale_y_continuous(labels = scales::percent, expand = c(0, 0))
+ggOut
+
+
+ggplot(theOut, aes(x = VS, y = Cost)) + geom_point(aes(col = Sigma))
+
+
+### What about a totally different approach
+test <- subset(theOut, theOut$VS > 0)
+dim(test)
+
+head(test)
+
+bestPar <- GetBestPar(
+        masterCD4 = MasterCD4_2015,
+        data = MasterData,
+        calibParamOut = CalibParamOut,
+        minErrorRun = minErrorRun)
+
+
+# Add columns to data.frame
+test$testing <- test$Rho / bestPar[["Rho"]]
+test$linkage <- test$Q / bestPar[["q"]]
+test$preRetention <- bestPar[["Kappa"]] / test$Kappa
+test$initiation <- test$Gamma / bestPar[["Gamma"]]
+test$adherence <- test$Sigma
+test$retention <- bestPar[["Omega"]] / test$Omega
+
+head(test)
+
+cols <- ggColorHue(6)
+
+ggplot(test, aes(x = testing, y = VS)) + geom_point(alpha = 0.01, col = cols[1]) +
+geom_point(aes(x = linkage, y = VS), col = cols[2], alpha = 0.01) +
+geom_point(aes(x = preRetention, y = VS), col = cols[3], alpha = 0.01) +
+geom_point(aes(x = initiation, y = VS), col = cols[4], alpha = 0.01) +
+geom_point(aes(x = adherence, y = VS), col = cols[5], alpha = 0.01) +
+geom_point(aes(x = retention, y = VS), col = cols[6], alpha = 0.01)
+
+
+ggplot(test) + stat_summary(aes(x = testing, y = VS), fun.y = "mean", colour = cols[1], size = 1, geom = "line", alpha = 0.5) +
+stat_summary(aes(x = linkage, y = VS), fun.y = "mean", colour = cols[2], size = 1, geom = "line", alpha = 0.5) +
+stat_summary(aes(x = preRetention, y = VS), fun.y = "mean", colour = cols[3], size = 1, geom = "line", alpha = 0.5) +
+stat_summary(aes(x = initiation, y = VS), fun.y = "mean", colour = cols[4], size = 1, geom = "line", alpha = 0.5) +
+stat_summary(aes(x = adherence, y = VS), fun.y = "mean", colour = cols[5], size = 1, geom = "line", alpha = 0.5) +
+stat_summary(aes(x = retention, y = VS), fun.y = "mean", colour = cols[6], size = 1, geom = "line", alpha = 0.5)
+
+ggplot(test) + stat_summary(aes(x = testing, y = VS), fun.y = "mean", colour = cols[1], size = 2, geom = "point") +
+stat_summary(aes(x = linkage, y = VS), fun.y = "mean", colour = cols[2], size = 2, geom = "point") +
+stat_summary(aes(x = preRetention, y = VS), fun.y = "mean", colour = cols[3], size = 2, geom = "point") +
+stat_summary(aes(x = initiation, y = VS), fun.y = "mean", colour = cols[4], size = 2, geom = "point") +
+stat_summary(aes(x = adherence, y = VS), fun.y = "mean", colour = cols[5], size = 2, geom = "point") +
+stat_summary(aes(x = retention, y = VS), fun.y = "mean", colour = cols[6], size = 2, geom = "point")
+
+geom_point(aes(x = linkage, y = VS), col = cols[2], alpha = 0) + stat_summary(aes(x = linkage, y = VS), fun.y = "mean", colour = cols[2], size = 2, geom = "point")
+geom_point(aes(x = preRetention, y = VS), col = cols[3], alpha = 0) +
+geom_point(aes(x = initiation, y = VS), col = cols[4], alpha = 0) +
+geom_point(aes(x = adherence, y = VS), col = cols[5], alpha = 0) +
+geom_point(aes(x = retention, y = VS), col = cols[6], alpha = 0)
+
+
+
+# ggplot(test[test$testing > 1,], aes(x = cumsum(VS))) + geom_density(col = "red") +
+# geom_density(data = test[test$adherence > 1,], aes(x = cumsum(VS)), col = "blue")
+
+
+# Density, strength vs. VS plot.
+
+
+ggplot(test, aes(VS)) + geom_histogram(aes(fill = ..count..), bins = 50)
+
+# Can we melt everything to see the combinations.
+dim(test)
+# hi <- test[1:10,]
+hi <- test
+
+reshape2::melt(data = hi,
+    id.vars = c('testing', 'linkage', 'preRetention', 'initiation', 'adherence', 'retention'),
+    variable.name = 'A',
+    value.name = 'VS')
+
+
+names(hi)
+
+for(i in 1:dim(hi)[1]) {
+    hi[i,'testing'] <- if(hi[i,'testing'] > 1) {TRUE} else {FALSE}
+    hi[i,'linkage'] <- if(hi[i,'linkage'] > 1) {TRUE} else {FALSE}
+    hi[i,'preRetention'] <- if(hi[i,'preRetention'] > 1) {TRUE} else {FALSE}
+    hi[i,'initiation'] <- if(hi[i,'initiation'] > 1) {TRUE} else {FALSE}
+    hi[i,'adherence'] <- if(hi[i,'adherence'] > 1) {TRUE} else {FALSE}
+    hi[i,'retention'] <- if(hi[i,'retention'] > 1) {TRUE} else {FALSE}
+}
+
+hi$intNumber <- seq(1, dim(hi)[1])
+
+hihi <- reshape2::melt(data = hi,
+    id.vars = c('testing', 'linkage', 'preRetention', 'initiation', 'adherence', 'retention'),
+    measure.vars = 'VS')
+
+
+for(i in 1:dim(hi)[1]) {
+    hi[i,"intNumber"] <- 0
+    hi[i,"intNumber"] <- if (hi[i,'testing'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'linkage'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'preRetention'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'initiation'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'adherence'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'retention'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+}
+
+ggplot(hi, aes(x = intNumber, y = VS)) + geom_point(aes(col = as.factor(testing)))
+
+as.factor(hi$testing)
+
+hi <- test[1:10,]
+dim(hi)
+
+hi
+
+# Number each intervention
+hi$intNumber <- seq(1, dim(hi)[1])
+
+hi
+
+#### penalty for multiple interventions. ####
+
+hi <- subset(theOut, theOut$VS > 0)
+# selectedResults <- hi
+
+bestPar <- GetBestPar(
+        masterCD4 = MasterCD4_2015,
+        data = MasterData,
+        calibParamOut = CalibParamOut,
+        minErrorRun = minErrorRun)
+
+
+# Add columns to data.frame
+hi$testing <- hi$Rho / bestPar[["Rho"]]
+hi$linkage <- hi$Q / bestPar[["q"]]
+hi$preRetention <- bestPar[["Kappa"]] / hi$Kappa
+hi$initiation <- hi$Gamma / bestPar[["Gamma"]]
+hi$adherence <- hi$Sigma
+hi$retention <- bestPar[["Omega"]] / hi$Omega
+
+test_time <- proc.time()
+for(i in 1:dim(hi)[1]) {
+    hi[i,"intNumber"] <- 0
+    hi[i,"intNumber"] <- if (hi[i,'testing'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'linkage'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'preRetention'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'initiation'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'adherence'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+    hi[i,"intNumber"] <- if (hi[i,'retention'] > 1) {hi[i,"intNumber"] + 1} else {hi[i,"intNumber"]}
+}
+proc.time() - test_time
+
+intervention <- c(
+    "Testing",
+    "Linkage",
+    "Pre-ART Retention",
+    "Initiation",
+    "Adherence",
+    "ART Retention")
+
+strength <- c(
+    sum( hi[hi$testing      > 1, "testing"]      * hi[hi$testing      > 1, "VS"] * (1 / hi[hi$testing > 1, "intNumber"]) ) / dim(hi)[1],
+    sum( hi[hi$linkage      > 1, "linkage"]      * hi[hi$linkage      > 1, "VS"] * (1 / hi[hi$linkage > 1, "intNumber"]) ) / dim(hi)[1],
+    sum( hi[hi$preRetention > 1, "preRetention"] * hi[hi$preRetention > 1, "VS"] * (1 / hi[hi$preRetention > 1, "intNumber"]) ) / dim(hi)[1],
+    sum( hi[hi$initiation   > 1, "initiation"]   * hi[hi$initiation   > 1, "VS"] * (1 / hi[hi$initiation > 1, "intNumber"]) ) / dim(hi)[1],
+    sum( hi[hi$adherence    > 1, "adherence"]    * hi[hi$adherence    > 1, "VS"] * (1 / hi[hi$adherence > 1, "intNumber"]) ) / dim(hi)[1],
+    sum( hi[hi$retention    > 1, "retention"]    * hi[hi$retention    > 1, "VS"] * (1 / hi[hi$retention > 1, "intNumber"]) ) / dim(hi)[1]
+)
+
+# build data.frame
+resultOut <- data.frame(intervention, strength)
+resultOut$intervention <- factor(resultOut$intervention, levels = intervention)
+
+ggplot(resultOut, aes(x = intervention, y = strength)) + geom_bar(stat = "identity")
 
