5052ed97047bdd76ca8c008568acd6e1a996c766
Jack Olney <jack.olney@me.com>
2016-06-13 09:55:03 +0100

    if model recalibrated, then projection plot render functions re-run

--- CascadeDashboard/server/projection/plot.R
+++ CascadeDashboard/server/projection/plot.R
@@ -1,79 +1,124 @@
 output$plotValidation_PLHIV <- renderPlot({
+    input$NEXT_calib
     GenYourCascadePlot(1)
-}, height = 400,
-   width = 'auto',
-   bg = "transparent")
+    },
+    height = 400,
+    width = 'auto',
+    bg = "transparent"
+)
 
 output$plotValidation_DIAG <- renderPlot({
+    input$NEXT_calib
     GenYourCascadePlot(2)
-}, height = 400,
-   width = 'auto',
-   bg = "transparent")
+    },
+    height = 400,
+    width = 'auto',
+    bg = "transparent"
+)
 
 output$plotValidation_CARE <- renderPlot({
+    input$NEXT_calib
     GenYourCascadePlot(3)
-}, height = 400,
-   width = 'auto',
-   bg = "transparent")
+    },
+    height = 400,
+    width = 'auto',
+    bg = "transparent"
+)
 
 output$plotValidation_ART <- renderPlot({
+    input$NEXT_calib
     GenYourCascadePlot(4)
-}, height = 400,
-   width = 'auto',
-   bg = "transparent")
+    },
+    height = 400,
+    width = 'auto',
+    bg = "transparent"
+)
 
 output$plotValidation_SUPP <- renderPlot({
+    input$NEXT_calib
     GenYourCascadePlot(5)
-}, height = 400,
-   width = 'auto',
-   bg = "transparent")
+    },
+    height = 400,
+    width = 'auto',
+    bg = "transparent"
+)
 
 output$plotCascade <- renderPlot({
+    input$NEXT_calib
     GenCascadePlot()
-}, height = 400,
-   width = 'auto')
+    },
+    height = 400,
+    width = 'auto'
+)
 
 output$plotPowersCascade <- renderPlot({
+    input$NEXT_calib
     GenPowersCascadePlot()
-}, height = 400,
-   width = 'auto')
+    },
+    height = 400,
+    width = 'auto'
+)
 
 output$plot909090 <- renderPlot({
+    input$NEXT_calib
     Gen909090Plot()
-}, height = 500,
-   width = 'auto')
+    },
+    height = 500,
+    width = 'auto'
+)
 
 output$plotNewInf <- renderPlot({
+    input$NEXT_calib
     GenNewInfPlot(wizard = FALSE)
-}, height = 500,
-   width = 'auto')
+    },
+    height = 500,
+    width = 'auto'
+)
 
 output$plotAidsDeaths <- renderPlot({
+    input$NEXT_calib
     GenAidsDeathsPlot(wizard = FALSE)
-}, height = 500,
-   width = 'auto')
+    },
+    height = 500,
+    width = 'auto'
+)
 
 output$plotSingle <- renderPlot({
+    input$NEXT_calib
     GenSinglePlot()
-}, height = 400,
-   width = 'auto')
+    },
+    height = 400,
+    width = 'auto'
+)
 
 output$plotAll <- renderPlot({
+    input$NEXT_calib
     GenAllPlot()
-}, height = 800,
-   width = 'auto')
+    },
+    height = 800,
+    width = 'auto'
+)
 
 output$plotNewInf_wizard <- renderPlot({
+    input$NEXT_calib
     GenNewInfPlot(wizard = TRUE)
-}, height = 240,
-   width = 'auto')
+    },
+    height = 240,
+    width = 'auto'
+)
 
 output$plotAidsDeaths_wizard <- renderPlot({
+    input$NEXT_calib
     GenAidsDeathsPlot(wizard = TRUE)
-}, height = 240,
-   width = 'auto')
+    },
+    height = 240,
+    width = 'auto'
+)
 
 output$plotCascade_wizard <- renderPlot({
+    input$NEXT_calib
     GenCascadePlot()
-}, height = 400,
-   width = 'auto')
+    },
+    height = 400,
+    width = 'auto'
+)
