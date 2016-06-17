3cf4064d10bb13c79c3170c449a3cb6286e590cc
Jack Olney <jack.olney@me.com>
2016-06-17 12:02:57 +0100

    upscaling adherence intervention so that we can actually achieve the 90-90-90 goals

--- CascadeDashboard/server/optimisation/observe-input.R
+++ CascadeDashboard/server/optimisation/observe-input.R
@@ -1,25 +1,25 @@
 ### Optimisation Input ###
 
 output$UI_opt_rho_MAX <- renderUI({
-    numericInput(inputId = "opt_rho_intValue", label = "Intervention Rate:",                       value = CalibParamMaxMin$rho_MAX,   min = 0, max = 10, step = 0.01, width = "100%")
+    numericInput(inputId = "opt_rho_intValue", label = "Intervention Rate:",                       value = CalibParamMaxMin$rho_MAX,   min = 0, max = 10, step = 0.01,  width = "100%")
 })
 
 output$UI_opt_q_MAX <- renderUI({
-    numericInput(inputId = "opt_q_intValue", label = "Maximum Intervention Proportion:",           value = CalibParamMaxMin$q_MAX,     min = 0, max = 1, step = 0.001, width = "100%")
+    numericInput(inputId = "opt_q_intValue", label = "Maximum Intervention Proportion:",           value = CalibParamMaxMin$q_MAX,     min = 0, max = 1, step = 0.001,  width = "100%")
 })
 
 output$UI_opt_kappa_MAX <- renderUI({
-    numericInput(inputId = "opt_kappa_intValue", label = "Intervention Rate (smaller is better):", value = CalibParamMaxMin$kappa_MIN, min = 0, max = 1, step = 0.001, width = "100%")
+    numericInput(inputId = "opt_kappa_intValue", label = "Intervention Rate (smaller is better):", value = CalibParamMaxMin$kappa_MIN, min = 0, max = 1, step = 0.001,  width = "100%")
 })
 
 output$UI_opt_gamma_MAX <- renderUI({
-    numericInput(inputId = "opt_gamma_intValue", label = "Intervention Rate:",                     value = CalibParamMaxMin$gamma_MAX, min = 0, max = 10, step = 0.01, width = "100%")
+    numericInput(inputId = "opt_gamma_intValue", label = "Intervention Rate:",                     value = CalibParamMaxMin$gamma_MAX, min = 0, max = 10, step = 0.01,  width = "100%")
 })
 
 output$UI_opt_sigma_MAX <- renderUI({
-    numericInput(inputId = "opt_sigma_intValue", label = "Intervention Rate (py^-1):",             value = 0.01,                       min = 0, max = 0.01, step = 0.01,  width = "100%")
+    numericInput(inputId = "opt_sigma_intValue", label = "Intervention Rate (py^-1):",             value = 0.5,                       min = 0, max = 0.5, step = 0.001, width = "100%")
 })
 
 output$UI_opt_omega_MAX <- renderUI({
-    numericInput(inputId = "opt_omega_intValue", label = "Intervention Rate:",                     value = CalibParamMaxMin$omega_MIN, min = 0, max = 1, step = 0.001, width = "100%")
+    numericInput(inputId = "opt_omega_intValue", label = "Intervention Rate:",                     value = CalibParamMaxMin$omega_MIN, min = 0, max = 1, step = 0.001,  width = "100%")
 })
