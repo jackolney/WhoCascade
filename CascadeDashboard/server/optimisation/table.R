55fd5e0458699b74d7f1106aa6742b8be84689f9
Jack Olney <jack.olney@me.com>
2016-06-20 15:21:59 +0100

    buttons to DT::datatable

--- CascadeDashboard/server/optimisation/table.R
+++ CascadeDashboard/server/optimisation/table.R
@@ -1,83 +1,12 @@
 # Data Table Render Functions
-output$optimDTout <- DT::renderDataTable({
-
-    input$NEXT_optIntro
-
-    # Subset data using opt_VS_cutoff
-    selectedResults <- subset(optResult, optResult$VS >= (input$opt_VS_cutoff / 100))
-
-    baseline <- CallBestModel(
-        CalibOut = CalibOut,
-        minErrorRun = minErrorRun)
-
-    alt <- CallBestModel(
-        CalibOut = CalibOut,
-        minErrorRun = minErrorRun,
-        Rho = mean(selectedResults$Rho),
-        q = mean(selectedResults$Q),
-        Kappa = mean(selectedResults$Kappa),
-        Gamma = mean(selectedResults$Gamma),
-        Sigma = mean(selectedResults$Sigma),
-        Omega = mean(selectedResults$Omega))
-
-    Intervention <- c(
-        "Testing",
-        "Linkage",
-        "Pre-ART Retention",
-        "Initiation",
-        "Adherence",
-        "ART Retention"
-    )
-
-    Description <- c(
-        "The number of additional individuals requiring diagnosis",
-        "The number of additional individuals that need to be linked to care",
-        "The number of additional individuals that need to be retained in pre-ART care",
-        "The number of additional individuals that need to be initiated onto treatment",
-        "The number of additional individuals that need to fully adhere to treatment",
-        "The number of additional individuals that need to be retained on ART"
-    )
-
-    # The values used in uiOutput()
-    Value <- c(
-        round(  (cumsum(alt$Dx)[251]           - alt$Dx[1]          ) - (cumsum(baseline$Dx)[251]   - baseline$Dx[1]    ),   digits = 0),
-        round(  (cumsum(alt$Care)[251]         - alt$Care[1]        ) - (cumsum(baseline$Care)[251] - baseline$Care[1]  ),   digits = 0),
-        round(  (cumsum(baseline$PreLtfu)[251] - baseline$PreLtfu[1]) - (cumsum(alt$PreLtfu)[251]   - alt$PreLtfu[1]    ),   digits = 0),
-        round(  (cumsum(alt$Tx)[251]           - alt$Tx[1]          ) - (cumsum(baseline$Tx)[251]   - baseline$Tx[1]    ),   digits = 0),
-        round(  (cumsum(alt$Vs)[251]           - alt$Vs[1]          ) - (cumsum(baseline$Vs)[251]   - baseline$Vs[1]    ),   digits = 0),
-        round(  (cumsum(baseline$Ltfu)[251]    - baseline$Ltfu[1]   ) - (cumsum(alt$Ltfu)[251]      - alt$Ltfu[1]       ),   digits = 0)
-    )
-
-    optimDT <- data.frame(Intervention, Description, Value)
-
-    DT::datatable(optimDT,
-        style = 'bootstrap',
-        rownames = FALSE,
-        options = list(
-            initComplete = JS(
-                "function(settings, json) {",
-                "$(this.api().table().header()).css({'background-color': '#4F8ABA', 'color': '#fff'});",
-                "}"),
-            autoWidth = FALSE,
-            columnDefs = list(list(className = 'dt-center', targets = 2))
-        )
-    ) %>% formatStyle(
-        columns = 'Intervention',
-        color = 'black',
-        fontWeight = 'bold'
-    ) %>% formatStyle(
-        columns = 'Value',
-        background = ColorFromMiddle(data = cbind(0, optimDT$Value), color1 = '#CF553D', color2 = '#46A55F'),
-        backgroundSize = '100% 88%',
-        backgroundRepeat = 'no-repeat',
-        backgroundPosition = 'center'
-    ) %>% formatCurrency(columns = 'Value', currency = '', interval = 3)
-})
 
 output$optimDTmodal <- DT::renderDataTable({
     return(datatable(Result_VS,
         style = 'bootstrap',
+        extensions = 'Buttons',
         options = list(
+            dom = 'Bfrtip',
+            buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
             pageLength = 25,
             autoWidth = FALSE,
             initComplete = JS(
@@ -106,7 +35,10 @@ output$optimDT909090modal <- DT::renderDataTable({
     # Shows the same values as optimDTmodal
     return(datatable(Result_VS,
         style = 'bootstrap',
+        extensions = 'Buttons',
         options = list(
+            dom = 'Bfrtip',
+            buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
             pageLength = 25,
             autoWidth = FALSE,
             initComplete = JS(
