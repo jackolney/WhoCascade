19439211fd1c167e4f8e209cb03c3ac7a863263c
Jack Olney <jack.olney@me.com>
2016-07-01 10:49:35 +0100

    model-document URL update

--- CascadeDashboard/ui/more/more_modeldocument.R
+++ CascadeDashboard/ui/more/more_modeldocument.R
@@ -1,3 +1,3 @@
 tabItem(tabName = "model_document",
-    HTML('<iframe src=\"https://drive.google.com/file/d/0B02uVauBTUwhckJ1bG1QRmdwTGM/preview\"style=\"border: 0; position:absolute; top:50px; left:0; right:0; width:100%; height:100%\"></iframe>')
+    HTML('<iframe src=\"https://drive.google.com/file/d/0B02uVauBTUwheTAzZUVkakwyVW8/preview\"style=\"border: 0; position:absolute; top:50px; left:0; right:0; width:100%; height:100%\"></iframe>')
     )
