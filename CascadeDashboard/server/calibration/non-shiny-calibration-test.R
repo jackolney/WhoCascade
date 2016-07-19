65121be5b90459e7d7a8c1f696af24cddb7fea33
Jack Olney <jack.olney@me.com>
2016-07-19 14:22:56 +0100

    setting the scene for unit tests

--- CascadeDashboard/server/calibration/non-shiny-calibration-test.R
+++ CascadeDashboard/server/calibration/non-shiny-calibration-test.R
@@ -39,56 +39,45 @@ source("server/calibration/non-shiny-calibration.R", local = FALSE)
 devtools::load_all(pkg = "~/git/WhoCascade/cascade")
 devtools::test(pkg = "~/git/WhoCascade/cascade")
 
-# Run baseline model (nothing fancy)
+## Run baseline model (nothing fancy)
+# Kenya
 KenyaData <- GetMasterDataSet("Kenya")
-
-TanzaniaData <- GetMasterDataSet("Tanzania")
-
-ZimbabweData <- GetMasterDataSet("Zimbabwe")
-
-# RUN CALIBRATION
 RunNSCalibration(country = "Kenya", data = KenyaData, maxIterations = 1e4, maxError = 2, limit = 100)
+BuildCalibrationPlotDetail(data = CalibOut, originalData = KenyaData, limit = 100)
 
+# Tanzania
+TanzaniaData <- GetMasterDataSet("Tanzania")
 RunNSCalibration(country = "Tanzania", data = TanzaniaData, maxIterations = 1e4, maxError = 2, limit = 100)
-
-RunNSCalibration(country = "Zimbabwe", data = ZimbabweData, maxIterations = 1e4, maxError = 2, limit = 100)
-
-# All elements should be present, now.
-
-# Is the minErrorRun = k or v
-
-# CalibOut might be longer than 100, then that needs k
-# else everything that is <100 in length needs v.
-
 BuildCalibrationPlotDetail(data = CalibOut, originalData = TanzaniaData, limit = 100)
 
-head(CalibParamOut)
+# Zimbabwe
+ZimbabweData <- GetMasterDataSet("Zimbabwe")
+RunNSCalibration(country = "Zimbabwe", data = ZimbabweData, maxIterations = 1e4, maxError = 2, limit = 100)
+BuildCalibrationPlotDetail(data = CalibOut, originalData = ZimbabweData, limit = 100)
 
-# We need to know the mean (and distribution of parameter values being used in the model)
+####################################################################################################
+####################################################################################################
+### TESTING OF WEIGHTS ###
 
-# hist(CalibParamOut$rho, col = "lightblue")
+KenyaData <- GetMasterDataSet("Kenya")
+KenyaData$calib$weight <- "green"
+RunNSCalibration(country = "Kenya", data = KenyaData, maxIterations = 1e4, maxError = 2, limit = 100)
+BuildCalibrationPlotDetail(data = CalibOut, originalData = KenyaData, limit = 100)
 
-# par(mfrow = c(2,4))
-# hist(CalibParamOut$rho, col = "lightblue")
-# hist(CalibParamOut$epsilon, col = "lightblue")
-# hist(CalibParamOut$kappa, col = "lightblue")
-# hist(CalibParamOut$gamma, col = "lightblue")
-# hist(CalibParamOut$theta, col = "lightblue")
-# hist(CalibParamOut$omega, col = "lightblue")
-# hist(CalibParamOut$p, col = "lightblue")
-# hist(CalibParamOut$q, col = "lightblue")
 
-# DefineParmRange(param = p, min = 0.01, max = 5)
+####################################################################################################
+####################################################################################################
 
+# Parameter Histograms (why don't I wrap all this crap in a function?)
 test <- as.data.frame(CalibParamOut)
-ggOne <-   ggplot(test, aes(rho)) +     geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggTwo <-   ggplot(test, aes(epsilon)) + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggThree <- ggplot(test, aes(kappa)) +   geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggFour <-  ggplot(test, aes(gamma)) +   geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggFive <-  ggplot(test, aes(theta)) +   geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggSix <-   ggplot(test, aes(omega)) +   geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggSeven <- ggplot(test, aes(p)) +       geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
-ggEight <- ggplot(test, aes(q)) +       geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggOne   <- ggplot(test, aes(rho))     + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggTwo   <- ggplot(test, aes(epsilon)) + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggThree <- ggplot(test, aes(kappa))   + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggFour  <- ggplot(test, aes(gamma))   + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggFive  <- ggplot(test, aes(theta))   + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggSix   <- ggplot(test, aes(omega))   + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggSeven <- ggplot(test, aes(p))       + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
+ggEight <- ggplot(test, aes(q))       + geom_histogram(aes(fill = ..count..), bins = 10) + theme_classic() + theme(legend.position = "none", text = element_text(family = "Avenir Next"), axis.line.x = element_line(), axis.line.y = element_line()) + scale_y_continuous(expand = c(0,0))
 
 gridExtra::grid.arrange(ggOne, ggTwo, ggThree, ggFour, ggFive, ggSix, ggSeven, ggEight, ncol = 4, nrow = 2)
 
