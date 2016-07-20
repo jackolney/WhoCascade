132b1cef7b9d326fb4f5eecb0fb3c2046b143949
Jack Olney <jack.olney11@imperial.ac.uk>
2016-07-20 17:59:00 +0100

    updates to notes

--- CascadeDashboard/server/country/observe.R
+++ CascadeDashboard/server/country/observe.R
@@ -5,6 +5,28 @@ observeEvent(input$new_country_name, {
     # new we need to 'undisable' the buttons ...
     # activate links to 'edit' pages. (conditional on input$new_country_name != "")
     # have some methods of populating the MasterData list()
+
+    # The below buttons are still looking at the current country selected... when a name is entered,
+    # we need to override this.
+    # So when a 'name' of country is entered, all boxes should go RED and be enabled to allow data to be entered
+
+
+    # _CASCADE_FLAG_
+    # How do we entere data for this?? (pretty much replicating the before methods)
+
+    #_CD4_FLAG_
+    # Enter CD4 distribution? Sliders? range(0,1)
+
+    # _Incidence_FLAG_
+    # values that update a plot?
+
+    # _Treatment_FLAG_
+    # We already have this framework, just adapt for a full page
+
+    # Does all this new work deprecate 'data-review'???
+
+    # I'm not sure that is does straight away,, maybe we can loop both in
+
     if(input$new_country_name != "") {
         updateButton(session, inputId = "_CASCADE_FLAG_",   disabled = FALSE)
         updateButton(session, inputId = "_CD4_FLAG_",       disabled = FALSE)
