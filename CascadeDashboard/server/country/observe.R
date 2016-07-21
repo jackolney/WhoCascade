c36001cb488b090af0fdbda5abd5984541d66b3d
Jack Olney <jack.olney11@imperial.ac.uk>
2016-07-21 11:05:58 +0100

    alert system update

--- CascadeDashboard/server/country/observe.R
+++ CascadeDashboard/server/country/observe.R
@@ -27,16 +27,27 @@ observeEvent(input$new_country_name, {
 
     # I'm not sure that is does straight away,, maybe we can loop both in
 
-    if(input$new_country_name != "") {
-        updateButton(session, inputId = "_CASCADE_FLAG_",   disabled = FALSE)
-        updateButton(session, inputId = "_CD4_FLAG_",       disabled = FALSE)
-        updateButton(session, inputId = "_Incidence_FLAG_", disabled = FALSE)
-        updateButton(session, inputId = "_Treatment_FLAG_", disabled = FALSE)
+    if(input$new_country_name == "") {
+        updateTextInput(session, inputId = "new_country_name", value = "")
+        updateButton(session,    inputId = "_CASCADE_FLAG_",   disabled = TRUE)
+        updateButton(session,    inputId = "_CD4_FLAG_",       disabled = TRUE)
+        updateButton(session,    inputId = "_Incidence_FLAG_", disabled = TRUE)
+        updateButton(session,    inputId = "_Treatment_FLAG_", disabled = TRUE)
     } else {
-        updateButton(session, inputId = "_CASCADE_FLAG_",   disabled = TRUE)
-        updateButton(session, inputId = "_CD4_FLAG_",       disabled = TRUE)
-        updateButton(session, inputId = "_Incidence_FLAG_", disabled = TRUE)
-        updateButton(session, inputId = "_Treatment_FLAG_", disabled = TRUE)
+        # These actually need to be CHECK functions (as when we edit the country name, then will all go red again)
+        updateButton(session, inputId = "_CASCADE_FLAG_",    style = "danger",  disabled = FALSE, icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+        updateButton(session, inputId = "_CD4_FLAG_",        style = "danger",  disabled = FALSE, icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+        updateButton(session, inputId = "_Incidence_FLAG_",  style = "danger",  disabled = FALSE, icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+        updateButton(session, inputId = "_Treatment_FLAG_",  style = "danger",  disabled = FALSE, icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+        shinyBS::closeAlert(session, alertId = "alertId_PROCEED")
+        shinyBS::createAlert(session,
+            anchorId = "_DONOTPROCEED_",
+            alertId = "alertId_DONOTPROCEED",
+            title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "DO NOT PROCEED"),
+            content = "The model has insufficient data to quantify the cascade. Please select another country.",
+            style = "danger",
+            dismiss = TRUE,
+            append = TRUE)
     }
 })
 
@@ -44,5 +55,66 @@ observeEvent(input$new_country_name, {
 observeEvent(input$NEW_country, {
     if(input$NEW_country == FALSE) {
         updateTextInput(session, inputId = "new_country_name", value = "")
+        shinyBS::closeAlert(session, alertId = "alertId_PROCEED")
+        shinyBS::createAlert(session,
+            anchorId = "_DONOTPROCEED_",
+            alertId = "alertId_DONOTPROCEED",
+            title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "DO NOT PROCEED"),
+            content = "The model has insufficient data to quantify the cascade. Please select another country.",
+            style = "danger",
+            dismiss = TRUE,
+            append = TRUE)
     }
 })
+
+
+# Need to replicate mission-control.R
+
+# if (CheckCSV_PLHIV(input$selectCountry) & CheckCSV_ART(input$selectCountry)) {
+#     updateButton(session, inputId = "_CASCADE_FLAG_",    style = "success", icon = icon("check", class = "fa-lg fa-fw", lib = "font-awesome"))
+# } else {
+#     updateButton(session, inputId = "_CASCADE_FLAG_",    style = "danger",  icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+# }
+
+# if (CheckCSV_CD4(input$selectCountry)) {
+#     updateButton(session, inputId = "_CD4_FLAG_",        style = "success", icon = icon("check", class = "fa-lg fa-fw", lib = "font-awesome"))
+# } else {
+#     updateButton(session, inputId = "_CD4_FLAG_",        style = "danger",  icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+# }
+
+# if (CheckCSV_Incidence(input$selectCountry)) {
+#     updateButton(session, inputId = "_Incidence_FLAG_",  style = "success", icon = icon("check", class = "fa-lg fa-fw", lib = "font-awesome"))
+# } else {
+#     updateButton(session, inputId = "_Incidence_FLAG_",  style = "danger",  icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+# }
+
+# if (CheckCSV_Treatment(input$selectCountry)) {
+#     updateButton(session, inputId = "_Treatment_FLAG_",  style = "success", icon = icon("check", class = "fa-lg fa-fw", lib = "font-awesome"))
+# } else {
+#     updateButton(session, inputId = "_Treatment_FLAG_",  style = "danger",  icon = icon("times", class = "fa-lg fa-fw", lib = "font-awesome"))
+# }
+
+# # If MasterData exists then destroy it, then re-assign.
+# if (exists("MasterData")) rm(MasterData, pos = ".GlobalEnv")
+# try(MasterData <<- GetMasterDataSet(input$selectCountry), silent = FALSE)
+# if (exists("MasterData")) {
+#     shinyBS::closeAlert(session, alertId = "alertId_DONOTPROCEED")
+#     shinyBS::createAlert(session,
+#         anchorId = "_PROCEED_",
+#         alertId = "alertId_PROCEED",
+#         title = paste(icon("check", class = "fa-lg fa-fw", lib = "font-awesome"), "PROCEED"),
+#         content = "The model has sufficient data to quantify the cascade.",
+#         style = "success",
+#         dismiss = TRUE,
+#         append = TRUE)
+# } else {
+#     shinyBS::closeAlert(session, alertId = "alertId_PROCEED")
+#     shinyBS::createAlert(session,
+#         anchorId = "_DONOTPROCEED_",
+#         alertId = "alertId_DONOTPROCEED",
+#         title = paste(icon("exclamation-triangle", class = "fa-lg fa-fw", lib = "font-awesome"), "DO NOT PROCEED"),
+#         content = "The model has insufficient data to quantify the cascade. Please select another country.",
+#         style = "danger",
+#         dismiss = TRUE,
+#         append = TRUE)
+# }
