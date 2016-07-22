22efc98fcdba6a07e6f2ce4da77c9a6e0aea111a
Jack Olney <jack.olney@me.com>
2016-07-22 10:34:07 +0100

    conditional formatting of edit-cascade table

--- CascadeDashboard/server/country/rhandsontable.R
+++ CascadeDashboard/server/country/rhandsontable.R
@@ -28,5 +28,16 @@ output$hot <- renderRHandsontable({
             hot_col(col = "country", readOnly = TRUE) %>%
             hot_col(col = "indicator", readOnly = TRUE) %>%
             hot_col(col = "year", type = "date", format = "%Y", readOnly = TRUE) %>%
-            hot_col(col = "weight", type = "dropdown", source = c("green", "amber", "red"), strict = TRUE, allowInvalid = FALSE, halign = "htLeft")
+            hot_col(col = "weight", type = "dropdown", source = c("green", "amber", "red"), default = as.character(NA), strict = TRUE, allowInvalid = FALSE, halign = "htLeft",
+                renderer = "function (instance, td, row, col, prop, value, cellProperties) {
+                    Handsontable.renderers.TextRenderer.apply(this, arguments);
+                    if (value == 'green') {
+                        td.style.background = 'green';
+                    } else if (value == 'amber') {
+                        td.style.background = 'orange';
+                    } else if (value == 'red') {
+                        td.style.background = 'red';
+                    }
+                }") %>%
+            hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE)
 })
