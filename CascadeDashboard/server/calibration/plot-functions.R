a75fb30eb27226fd16881fc4670a10d302b6413a
Jack Olney <jack.olney@me.com>
2016-08-08 14:32:33 +0100

    minor cascade plot tweak

--- CascadeDashboard/server/calibration/plot-functions.R
+++ CascadeDashboard/server/calibration/plot-functions.R
@@ -265,6 +265,7 @@ BuildCalibrationPlotComplex <- function(data, originalData) {
 
 BuildDataReviewPlot <- function(data) {
     data$indicator <- factor(data$indicator, levels = c("PLHIV", "PLHIV Diagnosed", "PLHIV in Care", "PLHIV on ART", "PLHIV Suppressed"))
+    data$year <- as.numeric(as.character(data$year))
     ggOut <- ggplot(data, aes(x = year, y = value))
     ggOut <- ggOut + geom_bar(aes(fill = indicator), stat = "identity", position = "dodge")
     ggOut <- ggOut + expand_limits(y = round(max(data$value), digits = -5))
@@ -273,17 +274,16 @@ BuildDataReviewPlot <- function(data) {
         breaks = base::pretty(seq(0, round(max(data$value), digits = -5)), n = 5),
         labels = scales::comma,
         expand = c(0, 0))
+    ggOut <- ggOut + theme(axis.ticks.x = element_blank())
     ggOut <- ggOut + scale_x_continuous(breaks = seq(2010, 2015, 1), labels = seq(2010, 2015, 1))
     ggOut <- ggOut + theme(axis.text.x = element_text(size = 14))
     ggOut <- ggOut + theme(axis.text.y = element_text(size = 14))
     ggOut <- ggOut + theme(axis.ticks.x = element_blank())
-    ggOut <- ggOut + theme(axis.title = element_text(size = 14))
     ggOut <- ggOut + theme(legend.text = element_text(size = 13))
     ggOut <- ggOut + theme(axis.line.x = element_line())
     ggOut <- ggOut + theme(axis.line.y = element_line())
-    ggOut <- ggOut + theme(axis.title.y = element_blank())
+    ggOut <- ggOut + theme(axis.title = element_blank())
     ggOut <- ggOut + theme(legend.title = element_blank())
-    ggOut <- ggOut + xlab("Year")
     ggOut <- ggOut + theme(text = element_text(family = "Avenir Next"))
     ggOut
 }
