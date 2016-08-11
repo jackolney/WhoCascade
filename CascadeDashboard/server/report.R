d5d32f4a5241da0ad1e33d658ec41c3655f6e1a5
Jack Olney <jack.olney@me.com>
2016-08-11 15:51:58 +0100

    tempdir() for report generation and images still permitted

--- CascadeDashboard/server/report.R
+++ CascadeDashboard/server/report.R
@@ -2,15 +2,20 @@ output$downloadReport <- downloadHandler(
     filename = 'cascade-report.pdf',
 
     content = function(file) {
+        # Identify absolute path of report.Rmd and any relevant images
         src <- normalizePath('server/report.Rmd')
-
-        # temporarily switch to the temp dir, in case you do not have write
-        # permission to the current working directory
-        # owd <- setwd(tempdir())
-        # on.exit(setwd(owd))
-        # file.copy(src, 'report.Rmd')
-
-        out <- render('server/report.Rmd', pdf_document())
+        srcImg <- normalizePath('www/ModelSimple.png')
+        # store original working directory in owd, but setwd() to tempdir()
+        owd <- setwd(tempdir())
+        # at end of function, setwd to original working directory
+        on.exit(setwd(owd))
+        # copy original report.Rmd to current working directory (temp)
+        file.copy(src, 'report.Rmd')
+        # copy any images that are used in the report to a the tempdir()
+        file.copy(srcImg, 'ModelSimple.png')
+        # call Rmarkdown::render
+        out <- render('report.Rmd', pdf_document())
+        # rename 'out' as 'file'
         file.rename(out, file)
     }
 )
