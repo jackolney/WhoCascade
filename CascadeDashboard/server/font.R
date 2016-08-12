30fa121421aa20ac20d5f4583f4eaf932c81bb76
Jack Olney <jack.olney@me.com>
2016-08-12 15:51:55 +0100

    reverting back to Avenir Next

--- CascadeDashboard/server/font.R
+++ CascadeDashboard/server/font.R
@@ -1,11 +1,14 @@
-# This allows webfonts to be used anywhere
-showtext.auto()
+# # This allows webfonts to be used anywhere
+# showtext.auto()
 
-# Add a nice slim font. Can use anything from: https://fonts.google.com
-font.add.google(name = "Source Sans Pro", family = "Source Sans Pro", repo = "google", regular.wt = 600, bold.wt = 1000)
-font.add.google(name = "Open Sans", family = "Open Sans", repo = "google")
+# # Add a nice slim font. Can use anything from: https://fonts.google.com
+# font.add.google(name = "Source Sans Pro", family = "Source Sans Pro", repo = "google", regular.wt = 600, bold.wt = 1000)
+# font.add.google(name = "Open Sans", family = "Open Sans", repo = "google")
+# font.add.google(name = "Oswald", family = "Oswald", repo = "google")
 
 # Set global default
-# figFont <- "Avenir Next"
+figFont <- "Avenir Next"
 # figFont <- "Source Sans Pro"
-figFont <- "Open Sans"
+# figFont <- "Open Sans"
+# figFont <- "Oswald"
+# figFont <- "Lato"
